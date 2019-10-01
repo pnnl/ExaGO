0f1f92410fee76a79a6bd3feea9e9351e72b6a51
Shri Abhyankar <shri@pnnl.gov>
2019-10-01 14:58:14 -0700

    CUDA-C-PROTOTYPE: Set up for bus residual calculation complete.

--- applications/pflow-proto.c
+++ applications/pflow-proto.c
@@ -13,7 +13,7 @@ busparams[i] = {# of gens
 		# status,Gff,Bff,Gft,Bft,Gtf,Btf,Gtt,Btt for each line
 
 */
-PetscErrorCode CreateParamArray(PFLOW pflow,double **busparamsarr)
+PetscErrorCode CreateParamArray(PFLOW pflow,double **busparamsarr,int **xidxarr,int **fidxarr)
 {
   PS ps=pflow->ps;
   PetscInt i,arr_size;
@@ -25,6 +25,7 @@ PetscErrorCode CreateParamArray(PFLOW pflow,double **busparamsarr)
   PSLOAD   load;
   PetscErrorCode ierr;
   double   *busparams;
+  int      *xidx,*fidx;
   PetscInt j;
 
   PetscFunctionBegin;
@@ -44,12 +45,17 @@ PetscErrorCode CreateParamArray(PFLOW pflow,double **busparamsarr)
 
     ierr = PetscPrintf(PETSC_COMM_SELF,"Bus [%d]: param size = %d\n",i,arr_size);CHKERRQ(ierr);
 
-    /* Create the parameter array for bus and pack it */
+    /* Create the parameter array for bus,xidx,fidx */
     ierr = PetscCalloc1(arr_size,&busparamsarr[i]);CHKERRQ(ierr);
+    ierr = PetscCalloc1(2+4*nconnlines,&xidxarr[i]);CHKERRQ(ierr);
+    ierr = PetscCalloc1(2,&fidxarr[i]);CHKERRQ(ierr);
+
     busparams = busparamsarr[i];
+    xidx      = xidxarr[i];
+    fidx      = fidxarr[i];
 
     /* # of gens, loads, and lines */
-    busparams[0]   = (double)bus->ngen;
+    busparams[0] = (double)bus->ngen;
     busparams[1] = (double)bus->nload;
     busparams[2] = (double)nconnlines;
 
@@ -83,8 +89,34 @@ PetscErrorCode CreateParamArray(PFLOW pflow,double **busparamsarr)
       busparams += 3;
     }
     
+    PetscInt locx;
+    ierr = PSBUSGetVariableLocation(bus,&locx);CHKERRQ(ierr);
+    xidx[0] = fidx[0] = locx;
+    xidx[1] = fidx[1] = locx+1;
+
+    xidx += 2;
+    
     for(j=0; j < nconnlines; j++) {
+      PSBUS busf,bust;
+      PetscInt locxf,locxt;
+      const PSBUS *connbuses;
+
       line = connlines[j];
+
+      ierr = PSLINEGetConnectedBuses(line,&connbuses);CHKERRQ(ierr);
+      busf = connbuses[0];
+      bust = connbuses[1];
+
+      ierr = PSBUSGetVariableLocation(busf,&locxf);CHKERRQ(ierr);
+      ierr = PSBUSGetVariableLocation(bust,&locxt);CHKERRQ(ierr);
+
+      xidx[0] = locxf;
+      xidx[1] = locxf+1;
+      xidx[2] = locxt;
+      xidx[3] = locxt+1;
+
+      xidx += 4;
+
       busparams[0] = (double)line->status;
       busparams[1] = line->yff[0];
       busparams[2] = line->yff[1];
@@ -102,6 +134,31 @@ PetscErrorCode CreateParamArray(PFLOW pflow,double **busparamsarr)
   PetscFunctionReturn(0);
 }
 
+int ComputeBusResidual(double *busparams,int *xidx,int *fidx,const double *x,double *f)
+{
+  
+  return 0;
+}
+PetscErrorCode ComputeGlobalResidual(PFLOW pflow,double **busparamsarr,int **xidxarr,int **fidxarr,Vec X, Vec F)
+{
+  PetscErrorCode ierr;
+  PS             ps=pflow->ps;
+  PetscInt       i;
+  const double   *x;
+  double         *f;
+
+  PetscFunctionBegin;
+  ierr = VecGetArrayRead(X,&x);CHKERRQ(ierr);
+  ierr = VecGetArray(F,&f);CHKERRQ(ierr);
+
+  for(i=0; i < ps->nbus; i++) {
+    ierr = ComputeBusResidual(busparamsarr[i],xidxarr[i],fidxarr[i],x,f);
+  }
+  ierr = VecRestoreArrayRead(X,&x);CHKERRQ(ierr);
+  ierr = VecRestoreArray(F,&f);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
 int main(int argc,char **argv)
 {
   PetscErrorCode ierr;
@@ -114,6 +171,10 @@ int main(int argc,char **argv)
   /* array of arrays to hold parameters needed by each bus to
      compute its residual */
   double            **busparams;
+  /* arrays to hold x locations needed by each bus to access the variables 
+     it needs to compute its residual */
+  int               **xidx,**fidx;
+  /* arrays to hold f locations where each bus needs to insert its residuals */
   
   PetscInitialize(&argc,&argv,"options/pflowoptions",help);
   
@@ -143,27 +204,35 @@ int main(int argc,char **argv)
   ierr = PFLOWCreateGlobalVector(pflow,&X);CHKERRQ(ierr);
   ierr = VecDuplicate(X,&F);CHKERRQ(ierr);
 
+  ierr = PetscLogStagePush(solve);CHKERRQ(ierr);
+  /* Solve */
+  ierr = PFLOWSolve(pflow);CHKERRQ(ierr);
+  PetscLogStagePop();
+
+  /* Update line flows, Pgen, Qgen, and other parameters */
+  ierr = PFLOWPostSolve(pflow);CHKERRQ(ierr);
+
+  PS ps = pflow->ps;
+
+  ierr = PetscCalloc1(ps->nbus,&busparams);CHKERRQ(ierr);
+  ierr = PetscCalloc1(ps->nbus,&xidx);CHKERRQ(ierr);
+  ierr = PetscCalloc1(ps->nbus,&fidx);CHKERRQ(ierr);
+
+  ierr = CreateParamArray(pflow,busparams,xidx,fidx);CHKERRQ(ierr);
+
   /* Set Initial Guess for X */
   ierr = PFLOWSetInitialGuess(pflow,X);CHKERRQ(ierr);
 
-  PS ps = pflow->ps;
-  ierr = PetscCalloc1(ps->nbus,&busparams);CHKERRQ(ierr);
-
-  ierr = CreateParamArray(pflow,busparams);CHKERRQ(ierr);
-
-
-  //  ierr = PetscLogStagePush(solve);CHKERRQ(ierr);
-  /* Solve */
-  //  ierr = PFLOWSolve(pflow);CHKERRQ(ierr);
-  //  PetscLogStagePop();
-
-  /* Update line flows, Pgen, Qgen, and other parameters */
-  //  ierr = PFLOWPostSolve(pflow);CHKERRQ(ierr);
+  ierr = ComputeGlobalResidual(pflow,busparams,xidx,fidx,X,F);CHKERRQ(ierr);
 
   for(PetscInt i=0; i < pflow->ps->nbus; i++) {
     ierr = PetscFree(busparams[i]);CHKERRQ(ierr);
+    ierr = PetscFree(xidx[i]);CHKERRQ(ierr);
+    ierr = PetscFree(fidx[i]);CHKERRQ(ierr);
   }
   ierr = PetscFree(busparams);CHKERRQ(ierr);
+  ierr = PetscFree(xidx);CHKERRQ(ierr);
+  ierr = PetscFree(fidx);CHKERRQ(ierr);
 
   ierr = VecDestroy(&X);CHKERRQ(ierr);
   ierr = VecDestroy(&F);CHKERRQ(ierr);
