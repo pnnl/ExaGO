71fcbdd288b31d08e32cdf3cfed705bd3508a548
Shri Abhyankar <shri@pnnl.gov>
2020-02-28 07:46:41 -0800

    Add inequality constraint bounds vectors for opflow-tao.

--- src/opflow/solver/tao/opflow-tao.h
+++ src/opflow/solver/tao/opflow-tao.h
@@ -7,5 +7,5 @@ struct _p_OPFLOWSolver_TAO {
   Tao nlp; /**< Tao solver */
   PetscBool converged;
 
-  PetscScalar obj_factor; /* objective scaling factor set by IPOPT for hessian */
+  Vec Glineq,Guineq; /* lower and upper bounds on inequality constraints */
 };
