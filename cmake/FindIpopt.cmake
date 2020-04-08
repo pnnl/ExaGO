169287c02b1fa752cb7fd1661bd3a7584bf70deb
Slaven Peles <slaven.peles@pnnl.gov>
2020-04-08 10:43:19 -0700

    Minor fixes to cmake, improvements to FindIpopt.

--- cmake/FindIpopt.cmake
+++ cmake/FindIpopt.cmake
@@ -1,6 +1,14 @@
 # If IPOPT_DIR is not set then try to find it at default locations
 if(NOT IPOPT_DIR)
-   find_path(IPOPT_DIR NAMES share/coin/doc/Ipopt/ipopt_addlibs_cpp.txt HINTS ~/local/ipopt)
+   find_path(IPOPT_DIR NAMES share/coin/doc/Ipopt/ipopt_addlibs_cpp.txt 
+     HINTS
+     /usr
+     /usr/local
+     /opt/local
+     ~/local/ipopt
+     # PNNL specific paths
+     /share/apps/ipopt/3.13.0
+   )
 endif()
 
 # Exit if IPOPT_DIR is not set and cannot be found in default locations
@@ -9,12 +17,20 @@ if(NOT IPOPT_DIR)
 endif(NOT IPOPT_DIR)
 
 # Ipopt library location
-find_library(IPOPT_LIBRARY NAME libipopt.a libipopt.so libipopt.dylib HINTS ${IPOPT_DIR}/lib)
+find_library(IPOPT_LIBRARY NAME libipopt.a libipopt.so libipopt.dylib 
+  HINTS 
+  ${IPOPT_DIR}/lib
+  ${IPOPT_DIR}/lib64
+)
 message(STATUS "IPOPT library directory=${IPOPT_LIBRARY}")
 
 # Ipopt include directories
 # Find Ipopt header path and ensure all needed files are there
-find_path(IPOPT_INCLUDE_DIR NAME IpIpoptNLP.hpp HINTS ${IPOPT_DIR}/include/coin)
+find_path(IPOPT_INCLUDE_DIR NAME IpIpoptNLP.hpp 
+  HINTS 
+  ${IPOPT_DIR}/include/coin
+  ${IPOPT_DIR}/include/coin-or
+)
 message(STATUS "IPOPT Include directory = ${IPOPT_INCLUDE_DIR}")
 
 if(IPOPT_INCLUDE_DIR AND IPOPT_LIBRARY)
