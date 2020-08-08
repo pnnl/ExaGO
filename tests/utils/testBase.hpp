791176006e838517722fe8624800e600a182e887
Shrirang <shrirang.abhyankar@pnnl.gov>
2020-08-08 13:27:15 -0700

    Revert "Merge branch 'opflow/unit-tests/new-hiop-interface' into 'master'"
    This reverts merge request !68

--- tests/utils/testBase.hpp
+++ tests/utils/testBase.hpp
@@ -28,15 +28,6 @@ namespace tests
 using LocalOrdinalType = int;
 using GlobalOrdinalType = long long int;
 using RealType = double;
-static const RealType zero = 0.0;
-static const RealType quarter = 0.25;
-static const RealType half = 0.5;
-static const RealType one = 1.0;
-static const RealType two = 2.0;
-static const RealType three = 3.0;
-static const RealType eps =
-    10*std::numeric_limits<RealType>::epsilon();
-static const LocalOrdinalType SKIP_TEST = -1;
 
 /**
   @brief Base class for all testing classes. Each child class will call the
@@ -44,7 +35,17 @@ static const LocalOrdinalType SKIP_TEST = -1;
  */
 class TestBase
 {
-protected:
+
+  static constexpr RealType zero = 0.0;
+  static constexpr RealType quarter = 0.25;
+  static constexpr RealType half = 0.5;
+  static constexpr RealType one = 1.0;
+  static constexpr RealType two = 2.0;
+  static constexpr RealType three = 3.0;
+  static constexpr RealType eps =
+      10*std::numeric_limits<RealType>::epsilon();
+  static constexpr LocalOrdinalType SKIP_TEST = -1;
+
   /*
    * must be const pointer and const dest for
    * const string declarations to pass
@@ -64,9 +65,9 @@ protected:
    */
   [[nodiscard]]
   static
-  LocalOrdinalType isEqual(const RealType value, const RealType reference, const RealType tol=eps)
+  LocalOrdinalType isEqual(const RealType value, const RealType reference)
   {
-    return (std::abs(value - reference)/(one + std::abs(reference)) < tol);
+    return (std::abs(value - reference)/(one + std::abs(reference)) < eps);
   }
 
   /// Prints error output for each rank
