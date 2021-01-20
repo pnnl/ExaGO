3bf8cddf3b77e2445d5456c6d496d15aa844e1ac
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2021-01-20 15:30:33 -0800

    using build scripts based on spack env
    Originally developed in functionality-tests-dev branch,
    these build scripts now use the spack environments
    developed in PNNL spack repository.

--- scripts/buildsystem/marianasVariables.sh
+++ scripts/buildsystem/marianasVariables.sh
@@ -3,23 +3,30 @@ module purge
 export MY_CLUSTER=marianas
 export PROJ_DIR=/qfs/projects/exasgd
 export APPS_DIR=/share/apps
+
+# When using system packages, we load them directly
 module load gcc/7.3.0
-module load cmake/3.15.3
 module load openmpi/3.1.3
-module load magma/2.5.2_cuda10.2
 module load cuda/10.2.89
-module load metis/5.1.0
-export MY_PETSC_DIR=$PROJ_DIR/$MY_CLUSTER/petsc
-export MY_UMPIRE_DIR=$PROJ_DIR/$MY_CLUSTER/umpire
-export MY_RAJA_DIR=$PROJ_DIR/$MY_CLUSTER/raja
-export MY_HIOP_DIR=$PROJ_DIR/$MY_CLUSTER/hiop-gpu-new-interface
-export MY_UMPIRE_DIR=$PROJ_DIR/$MY_CLUSTER/umpire
-export MY_IPOPT_DIR=$PROJ_DIR/$MY_CLUSTER/ipopt
-export MY_MAGMA_DIR=$APPS_DIR/magma/2.5.2/cuda10.2
+module load cmake/3.15.3
+
+# Configure spack
+source $PROJ_DIR/src/spack/share/spack/setup-env.sh
+spack env activate exago-ci --without-view
+
+# When loading spack packages, use spack and not modulefiles directly
+spack load petsc+mpi umpire+cuda raja+cuda hiop+cuda+mpi ipopt magma openmpi metis parmetis camp+cuda
+
+export MY_PETSC_DIR=`spack location -i petsc+mpi`
+export MY_RAJA_DIR=`spack location -i raja+cuda`
+export MY_HIOP_DIR=`spack location -i hiop+cuda+mpi`
+export MY_UMPIRE_DIR=`spack location -i umpire+cuda`
+export MY_IPOPT_DIR=`spack location -i ipopt`
+export MY_MAGMA_DIR=`spack location -i magma`
 export NVBLAS_CONFIG_FILE=$PROJ_DIR/$MY_CLUSTER/nvblas.conf
+export extraCmakeArgs="$extraCmakeArgs -DHIOP_NVCC_ARCH=sm_60"
 
 # Variables specified only for build matrix:
-export SPACK_INSTALL_DIR=$PROJ_DIR/spack-install/linux-centos7-broadwell/gcc-7.3.0
-export MY_UMPIRECPU_DIR=$PROJ_DIR/$MY_CLUSTER/umpire-cpu
-export MY_PETSC_NOMPI_DIR=$SPACK_INSTALL_DIR/petsc/3.14.1-mzxn4
-export MY_HIOP_NOMPI_DIR=$PROJ_DIR/$MY_CLUSTER/hiop-gpu-nompi-new-interface
+export MY_UMPIRECPU_DIR=`spack location -i umpire~cuda`
+export MY_PETSC_NOMPI_DIR=`spack location -i petsc~mpi`
+export MY_HIOP_NOMPI_DIR=`spack location -i hiop~mpi`
