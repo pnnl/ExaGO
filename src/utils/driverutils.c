425d01a6b2639b7d0c3164efe55fcc6f5bb8eef0
Amatya, Vinay C <vinay.amatya@pnnl.gov>
2021-03-22 21:16:08 +0000

    Resolve indeterministic stat failure in CI
    For some reason stat would indeterministically fail when checking fo the existence
    of the file path "... -options_file not passed". This commit hard codes that file
    path to fail in the hope that it fixes the bug in CI.
    
    Fix a typo.

--- src/utils/driverutils.c
+++ src/utils/driverutils.c
@@ -9,12 +9,122 @@ const char* ExaGOVerbosityNames[] = {"INFO","WARNING","ERROR"};
 
 static char ExaGOCurrentAppName[128];
 
-PetscErrorCode ExaGOInitialize(MPI_Comm comm, int *argc, char ***argv,
-    char* appname, char *help)
+PetscErrorCode ExagoHelpPrintf(MPI_Comm comm, const char appname[])
 {
   PetscErrorCode ierr;
 
+  char* versionstr;
+  ierr=ExaGOVersionGetFullVersionInfo(&versionstr);CHKERRQ(ierr);
+  fprintf(stderr, "===================================================================\n");
+  fprintf(stderr, "ExaGO Version Info:\n\n%s", versionstr); 
+  if (strcmp(appname,"opflow") == 0)
+  {
+    fprintf(stderr, "============== Help Options for application %s ==============\n\n", appname); 
+    fprintf(stderr, " General usage: mpiexec -n <N> ./%s <options>\n", appname);
+    fprintf(stderr, " Options:\n");
+    fprintf(stderr, "\t -netfile <netfilename>\n");
+    fprintf(stderr, "\t -opflow_model <POWER_BALANCE_POLAR|...>\n");
+    fprintf(stderr, "\t -opflow_solver <IPOPT|...>\n");
+    fprintf(stderr, "\t -opflow_initialization <MIDPOINT|...>\n");
+    fprintf(stderr, "\t -opflow_ignore_lineflow_constraints <0|1>\n");
+    fprintf(stderr, "\t -opflow_include_loadloss_variables <0|1>\n");
+    fprintf(stderr, "\t -opflow_include_powerimbalance_variables <0|1>\n");
+    fprintf(stderr, "\t -opflow_loadloss_penalty <Penalty ($)>\n");
+    fprintf(stderr, "\t -opflow_powerimbalance_penalty <Penalty ($)>\n");
+    fprintf(stderr, "\t -opflow_genbusvoltage <FIXED_WITHIN_QBOUNDS|...>\n");
+    fprintf(stderr, "\t -opflow_has_gensetpoint <0|1>\n");
+    fprintf(stderr, "\t -opflow_objective <MIN_GEN_COST|...>\n");
+    fprintf(stderr, "\t -opflow_use_agc <0|1>\n");
+    fprintf(stderr, "\t -opflow_tolerance <1e-6|...>\n");
+    fprintf(stderr, "\t -hiop_compute_mode <hybrid|...>\n");
+    fprintf(stderr, "\t -hiop_verbosity_level <0-10>\n");
+    fprintf(stderr, "\t -hiop_tolerance <1e-6|...>\n");
+    fprintf(stderr, "\t -print_output <0|1>\n");
+    fprintf(stderr, "\t -save_output <0|1>\n");
+    fprintf(stderr, "\n");
+  } 
+  else if (strcmp(appname,"pflow")==0) 
+  {
+    fprintf(stderr, "============== Help Options for application %s ==============\n", appname);
+  } 
+  else if (strcmp(appname,"sopflow") == 0) 
+  {
+    fprintf(stderr, "============== Help Options for application %s ==============\n", appname);
+    fprintf(stderr, " General usage: mpiexec -n <N> ./%s <options>\n", appname);
+    fprintf(stderr, " Options:\n");
+    fprintf(stderr, "\t -netfile <netfilename>\n");
+    fprintf(stderr, "\t -ctgcfile <ctgcfilename>\n");
+    fprintf(stderr, "\t -scenfile <scenfilename>\n");
+    fprintf(stderr, "\t -opflow_model <POWER_BALANCE_POLAR|...>\n");
+    fprintf(stderr, "\t -sopflow_solver <IPOPT|...>\n");
+    fprintf(stderr, "\t -sopflow_mode <0|1>\n");
+    fprintf(stderr, "\t -sopflow_Ns <Ns>\n");
+    fprintf(stderr, "\t -sopflow_enable_multicontingency <0|1>\n");
+    fprintf(stderr, "\t -scopflow_enable_multiperiod <0|1>\n");
+    fprintf(stderr, "\t -sopflow_tolerance <1e-6|...>\n");
+    fprintf(stderr, "\n");
+  }
+  else if (strcmp(appname,"scopflow") == 0) 
+  {
+    fprintf(stderr, "============== Help Options for application %s ==============\n", appname);
+    fprintf(stderr, " General usage: mpiexec -n <N> ./%s <options>\n", appname);
+    fprintf(stderr, " Options:\n");
+    fprintf(stderr, "\t -netfile <netfilename>\n");
+    fprintf(stderr, "\t -ctgcfile <ctgcfilename>\n");
+    fprintf(stderr, "\t -scopflow_windgenprofile <windgenproffilename>\n");
+    fprintf(stderr, "\t -scopflow_ploadprofile <ploadprofile_filename>\n");
+    fprintf(stderr, "\t -scopflow_qloadprofile <qloadprofile_filename>\n");
+    fprintf(stderr, "\t -opflow_model <POWER_BALANCE_POLAR|...>\n");
+    fprintf(stderr, "\t -scopflow_solver <IPOPT|...>\n");
+    fprintf(stderr, "\t -scopflow_mode <0|1>\n");
+    fprintf(stderr, "\t -sopflow_Nc <Nc>\n");
+    fprintf(stderr, "\t -scopflow_enable_multiperiod <0|1>\n");
+    fprintf(stderr, "\t -scopflow_duration <Hours>\n");
+    fprintf(stderr, "\t -scopflow_dT <Minutes>\n");
+    fprintf(stderr, "\t -scopflow_tolerance <1e-6|...>\n");
+    fprintf(stderr, "\n");
+
+  }
+  else if (strcmp(appname,"tcopflow") == 0) 
+  {
+    fprintf(stderr, "============== Help Options for application %s ==============\n", appname);
+    fprintf(stderr, " General usage: mpiexec -n <N> ./%s <options>\n", appname);
+    fprintf(stderr, " Options:\n");
+    fprintf(stderr, "\t -netfile <netfilename>\n");
+    fprintf(stderr, "\t -options_file <tcopflowoptionsfilename>\n");
+    fprintf(stderr, "\t -tcopflow_windgenprofile <windgenproffilename>\n");
+    fprintf(stderr, "\t -tcopflow_ploadprofile <ploadprofile_filename>\n");
+    fprintf(stderr, "\t -tcopflow_qloadprofile <qloadprofile_filename>\n");
+    fprintf(stderr, "\t -tcopflow_iscoupling <0|1>\n");
+    fprintf(stderr, "\t -opflow_ignore_lineflow_constraints <0|1>\n");
+    fprintf(stderr, "\t -tcopflow_duration <Hours>\n");
+    fprintf(stderr, "\t -tcopflow_dT <Minutes>\n");
+    fprintf(stderr, "\t -tcopflow_tolerance <1e-6|...>\n");
+    fprintf(stderr, "\n");
+  }
+  else 
+  {
+    fprintf(stderr, "Please enter a valid application name.\n");
+  }
+	exit(0);
+}
+
+PetscErrorCode ExaGOInitialize(MPI_Comm comm, int *argc, char ***argv,
+    char* appname, char *help)
+{
+  int i;
+  PetscErrorCode ierr;
+
   strcpy(ExaGOCurrentAppName,appname);
+  for(i=1; i<*argc; i++)
+  {
+    // Added null terminator here as this would cause errors with
+    if ((strncmp((*argv)[i],"-help\0",5)==0) || 
+        (strncmp((*argv)[i],"-h\0",3)==0))
+    {
+      PetscHelpPrintf = ExagoHelpPrintf(MPI_COMM_NULL, appname);
+    }
+  }
 
   int initialized;
   MPI_Initialized(&initialized);
@@ -34,7 +144,6 @@ PetscErrorCode ExaGOInitialize(MPI_Comm comm, int *argc, char ***argv,
   if(fp==NULL)
     ExaGOLogSetLoggingFilePointer(stderr);
 
-  int i;
   char options_pathname[200] = EXAGO_OPTIONS_DIR;
   char filename[64];
   ierr=sprintf(filename,"%soptions",ExaGOCurrentAppName);
