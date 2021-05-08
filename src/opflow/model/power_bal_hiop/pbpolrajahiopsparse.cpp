9a5c4dbef29a6ca0549cf354ec44b9fb5b549716
Abhyankar, Shrirang G <shrirang.abhyankar@pnnl.gov>
2021-05-08 19:55:26 +0000

    WIP - HIOP Sparse solver with GPU model
    OPFLOW: Started work on support for HIOP sparse solver interface for GPUs.
    
    Added a copy of hiop sparse solver interface.
    
    OPFLOW: Added model skeleton for GPU sparse version (copying from pbpolrajahiop)
    
    Fixed build
    
    Did some copy paste to add a test for HIOPSPARSE. This test is not actually
    functional yet.
    
    Started updating the hiopsparse model and solver code.
    
    More work on updating the solver and model
    
    Added scalar and vector unit tests for model to be used with HIOP sparse solver on GPU
    
    Apply cmake lint
    
    Fix unit tests.
    
    Set the size of array when using Umpire memset.

---
+++ src/opflow/model/power_bal_hiop/pbpolrajahiopsparse.cpp
@@ -0,0 +1,287 @@
+#include <exago_config.h>
+
+#if defined(EXAGO_ENABLE_RAJA)
+#if defined(EXAGO_ENABLE_HIOP_SPARSE)
+
+#include <private/opflowimpl.h>
+#include "pbpolrajahiopsparsekernels.hpp"
+
+/* Initialization is done on the host through this function. Copying over values to the device is done in OPFLOWSetInitialGuessArray_PBPOLRAJAHIOPSPARSE
+*/
+extern PetscErrorCode OPFLOWSetInitialGuess_PBPOL(OPFLOW,Vec);
+
+PetscErrorCode OPFLOWSetInitialGuess_PBPOLRAJAHIOPSPARSE(OPFLOW opflow,Vec X)
+{
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+
+  ierr = OPFLOWSetInitialGuess_PBPOL(opflow,X);CHKERRQ(ierr);
+
+  PetscFunctionReturn(0);
+}
+
+extern PetscErrorCode OPFLOWSetConstraintBounds_PBPOL(OPFLOW,Vec,Vec);
+
+/* The constraint bounds are also calculated on the host.
+*/
+PetscErrorCode OPFLOWSetConstraintBounds_PBPOLRAJAHIOPSPARSE(OPFLOW opflow,Vec Gl,Vec Gu)
+{
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = OPFLOWSetConstraintBounds_PBPOL(opflow,Gl,Gu);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+extern PetscErrorCode OPFLOWSetVariableBounds_PBPOL(OPFLOW,Vec,Vec);
+
+/* The variable bounds are also calculated on the host.
+*/
+PetscErrorCode OPFLOWSetVariableBounds_PBPOLRAJAHIOPSPARSE(OPFLOW opflow,Vec Xl,Vec Xu)
+{
+  PetscErrorCode ierr;
+  double         *xl,*xu,*xlt,*xut;
+  Vec            Xlt,Xut;
+  int            i;
+
+  PetscFunctionBegin;
+  ierr = OPFLOWSetVariableBounds_PBPOL(opflow,Xl,Xu);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+PetscErrorCode OPFLOWSolutionToPS_PBPOLRAJAHIOPSPARSE(OPFLOW opflow)
+{
+  PetscErrorCode ierr;
+  PS             ps=(PS)opflow->ps;
+  PetscInt       i,k;
+  Vec            X,Lambda;
+  PSBUS          bus;
+  PSGEN          gen;
+  PSLOAD         load;
+  PSLINE         line;
+  const PetscScalar *x,*lambda,*lambdae,*lambdai;
+  PetscInt       loc,gloc=0;
+  PetscScalar    Gff,Bff,Gft,Bft,Gtf,Btf,Gtt,Btt;
+  PetscScalar    Vmf,Vmt,thetaf,thetat,thetaft,thetatf;
+  PetscScalar    Pf,Qf,Pt,Qt;
+  PSBUS          busf,bust;
+  const PSBUS    *connbuses;
+  PetscInt       xlocf,xloct;
+
+  PetscFunctionBegin;
+
+  ierr = OPFLOWGetSolution(opflow,&X);CHKERRQ(ierr);
+  ierr = OPFLOWGetConstraintMultipliers(opflow,&Lambda);CHKERRQ(ierr);
+
+  ierr = VecGetArrayRead(X,&x);CHKERRQ(ierr);
+  ierr = VecGetArrayRead(Lambda,&lambda);CHKERRQ(ierr);
+  lambdae = lambda;
+  if(opflow->Nconineq) {
+    lambdai = lambdae + opflow->nconeq;
+  }
+
+  for(i=0; i < ps->nbus; i++) {
+    bus = &ps->bus[i];
+
+    loc = bus->startxVloc;
+
+    bus->va = x[loc];
+    bus->vm = x[loc+1];
+
+    gloc = bus->starteqloc;
+    bus->mult_pmis = lambdae[gloc];
+    bus->mult_qmis = lambdae[gloc+1];
+
+    if(opflow->include_powerimbalance_variables) {
+      loc = bus->startxpimbloc;
+      bus->pimb = x[loc];
+      bus->qimb = x[loc+1];
+    }
+
+
+    for(k=0; k < bus->ngen; k++) {
+      ierr = PSBUSGetGen(bus,k,&gen);CHKERRQ(ierr);
+      if(!gen->status) {
+	gen->pg = gen->qg = 0.0;
+	continue;
+      }
+      loc = gen->startxpowloc;
+
+      gen->pg = x[loc];
+      gen->qg = x[loc+1];
+
+      if(opflow->has_gensetpoint) {
+	gloc += gen->nconeq;
+      }
+    }
+
+    if(opflow->include_loadloss_variables) {
+      for(k=0; k < bus->nload; k++) {
+	ierr = PSBUSGetLoad(bus,k,&load);CHKERRQ(ierr);
+	loc = load->startxloadlossloc;
+	load->pl = load->pl - x[loc];
+	load->ql = load->ql - x[loc+1];
+      }
+    }
+  }
+
+  if(!opflow->ignore_lineflow_constraints) {
+    for(i=0; i<ps->nline; i++) {
+      line = &ps->line[i];
+      if(!line->status) {
+	line->mult_sf = line->mult_st = 0.0;
+	continue;
+      }
+      
+      Gff = line->yff[0];
+      Bff = line->yff[1];
+      Gft = line->yft[0];
+      Bft = line->yft[1];
+      Gtf = line->ytf[0];
+      Btf = line->ytf[1];
+      Gtt = line->ytt[0];
+      Btt = line->ytt[1];
+      
+      ierr = PSLINEGetConnectedBuses(line,&connbuses);CHKERRQ(ierr);
+      busf = connbuses[0];
+      bust = connbuses[1];
+      
+      xlocf = busf->startxVloc;
+      xloct = bust->startxVloc;
+
+      thetaf  = x[xlocf];
+      Vmf     = x[xlocf+1];
+      thetat  = x[xloct];
+      Vmt     = x[xloct+1];
+      thetaft = thetaf - thetat;
+      thetatf = thetat - thetaf;
+      
+      Pf = Gff*Vmf*Vmf  + Vmf*Vmt*(Gft*cos(thetaft) + Bft*sin(thetaft));
+      Qf = -Bff*Vmf*Vmf + Vmf*Vmt*(-Bft*cos(thetaft) + Gft*sin(thetaft));
+      
+      Pt = Gtt*Vmt*Vmt  + Vmt*Vmf*(Gtf*cos(thetatf) + Btf*sin(thetatf));
+      Qt = -Btt*Vmt*Vmt + Vmt*Vmf*(-Btf*cos(thetatf) + Gtf*sin(thetatf));
+
+      line->pf = Pf;
+      line->qf = Qf;
+      line->pt = Pt;
+      line->qt = Qt;
+      line->sf = PetscSqrtScalar(Pf*Pf + Qf*Qf);
+      line->st = PetscSqrtScalar(Pt*Pt + Qt*Qt);
+
+      if(line->rateA > 1e5) {
+	line->mult_sf = line->mult_st = 0.0;
+      } else {
+	gloc = line->startineqloc;
+	line->mult_sf = lambdai[gloc];
+	line->mult_st = lambdai[gloc+1];
+      }
+    }
+  }
+
+  ierr = VecRestoreArrayRead(X,&x);CHKERRQ(ierr);
+  ierr = VecRestoreArrayRead(Lambda,&lambda);CHKERRQ(ierr);
+
+  PetscFunctionReturn(0);
+}
+
+/* Reuse PBPOL model set up for obtaining locations */
+extern PetscErrorCode OPFLOWModelSetUp_PBPOL(OPFLOW);
+
+PetscErrorCode OPFLOWModelSetUp_PBPOLRAJAHIOPSPARSE(OPFLOW opflow)
+{
+  PetscErrorCode ierr;
+  PbpolModelRajaHiopSparse* pbpolrajahiopsparse = reinterpret_cast<PbpolModelRajaHiopSparse*>(opflow->model);
+  
+  PetscFunctionBegin;
+
+  ierr = OPFLOWModelSetUp_PBPOL(opflow);CHKERRQ(ierr);
+
+  ierr = pbpolrajahiopsparse->busparams.allocate(opflow);
+  ierr = pbpolrajahiopsparse->genparams.allocate(opflow);
+  ierr = pbpolrajahiopsparse->lineparams.allocate(opflow);
+  ierr = pbpolrajahiopsparse->loadparams.allocate(opflow);
+
+  BUSParamsRajahiopsparse  *busparams=&pbpolrajahiopsparse->busparams;
+  GENParamsRajahiopsparse  *genparams=&pbpolrajahiopsparse->genparams;
+  LOADParamsRajahiopsparse *loadparams=&pbpolrajahiopsparse->loadparams;
+  LINEParamsRajahiopsparse *lineparams=&pbpolrajahiopsparse->lineparams;
+
+  /* Need to compute the number of nonzeros in equality, inequality constraint Jacobians and Hessian */
+  int nnz_eqjacsp=0,nnz_ineqjacsp=0,nnz_hesssp=0;
+  opflow->nnz_eqjacsp = nnz_eqjacsp;
+  opflow->nnz_ineqjacsp = nnz_ineqjacsp;
+  opflow->nnz_hesssp = nnz_hesssp;
+
+  ierr = pbpolrajahiopsparse->busparams.copy(opflow);
+  ierr = pbpolrajahiopsparse->genparams.copy(opflow);
+  ierr = pbpolrajahiopsparse->lineparams.copy(opflow);
+  ierr = pbpolrajahiopsparse->loadparams.copy(opflow);
+
+  PetscFunctionReturn(0);
+}
+
+PetscErrorCode OPFLOWModelDestroy_PBPOLRAJAHIOPSPARSE(OPFLOW opflow)
+{
+  PetscErrorCode ierr;
+  PbpolModelRajaHiopSparse* pbpolrajahiopsparse = reinterpret_cast<PbpolModelRajaHiopSparse*>(opflow->model);
+
+  PetscFunctionBegin;
+  pbpolrajahiopsparse->destroy(opflow);
+  delete pbpolrajahiopsparse;
+  pbpolrajahiopsparse = nullptr;
+
+  PetscFunctionReturn(0);
+}
+
+/* reuse numvariables and numconstraints functions from PBPOL model */
+extern PetscErrorCode OPFLOWModelSetNumVariables_PBPOL(OPFLOW,PetscInt*,PetscInt*,PetscInt*);
+extern PetscErrorCode OPFLOWModelSetNumConstraints_PBPOL(OPFLOW,PetscInt*,PetscInt*,PetscInt*,PetscInt*);
+extern PetscErrorCode OPFLOWComputeEqualityConstraintJacobian_PBPOL(OPFLOW,Vec,Mat);
+extern PetscErrorCode OPFLOWComputeInequalityConstraintJacobian_PBPOL(OPFLOW,Vec,Mat);
+extern PetscErrorCode OPFLOWComputeHessian_PBPOL(OPFLOW,Vec,Vec,Vec,Mat);
+
+PetscErrorCode OPFLOWModelCreate_PBPOLRAJAHIOPSPARSE(OPFLOW opflow)
+{
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  
+  PbpolModelRajaHiopSparse* pbpol = new PbpolModelRajaHiopSparse(opflow);
+
+  opflow->model = pbpol;
+
+  opflow->spdnordering = PETSC_TRUE;
+
+  /* Inherit Ops */
+  opflow->modelops.destroy                              = OPFLOWModelDestroy_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.setnumvariables                      = OPFLOWModelSetNumVariables_PBPOL;
+  opflow->modelops.setnumconstraints                    = OPFLOWModelSetNumConstraints_PBPOL;
+  opflow->modelops.setvariablebounds                    = OPFLOWSetVariableBounds_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.setvariableboundsarray               = OPFLOWSetVariableBoundsArray_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.setconstraintbounds                  = OPFLOWSetConstraintBounds_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.setconstraintboundsarray             = OPFLOWSetConstraintBoundsArray_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.setinitialguess                      = OPFLOWSetInitialGuess_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.setinitialguessarray                 = OPFLOWSetInitialGuessArray_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.computeequalityconstraintsarray      = OPFLOWComputeEqualityConstraintsArray_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.computeinequalityconstraintsarray    = OPFLOWComputeInequalityConstraintsArray_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.computeobjectivearray                = OPFLOWComputeObjectiveArray_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.computegradientarray                 = OPFLOWComputeGradientArray_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.solutiontops                         = OPFLOWSolutionToPS_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.setup                                = OPFLOWModelSetUp_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.computeequalityconstraintjacobian    = OPFLOWComputeEqualityConstraintJacobian_PBPOL;
+  opflow->modelops.computeinequalityconstraintjacobian   = OPFLOWComputeInequalityConstraintJacobian_PBPOL;
+  opflow->modelops.computesparseequalityconstraintjacobianhiop            = OPFLOWComputeSparseEqualityConstraintJacobian_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.computesparseinequalityconstraintjacobianhiop            = OPFLOWComputeSparseInequalityConstraintJacobian_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.computesparsehessianhiop             = OPFLOWComputeSparseHessian_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.computedenseequalityconstraintjacobianhiop = OPFLOWComputeDenseEqualityConstraintJacobian_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.computedenseinequalityconstraintjacobianhiop = OPFLOWComputeDenseInequalityConstraintJacobian_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.computedensehessianhiop    = OPFLOWComputeDenseHessian_PBPOLRAJAHIOPSPARSE;
+  opflow->modelops.computehessian             = OPFLOWComputeHessian_PBPOL;
+  
+  PetscFunctionReturn(0);
+}
+
+#endif
+#endif
---
+++ src/opflow/model/power_bal_hiop/pbpolrajahiopsparse.hpp
@@ -0,0 +1,30 @@
+#include <exago_config.h>
+
+#if defined(EXAGO_ENABLE_RAJA)
+#if defined(EXAGO_ENABLE_HIOP_SPARSE)
+
+#ifndef _PBPOLRAJAHIOPSPARSE_H
+#define _PBPOLRAJAHIOPSPARSE_H
+
+#include <opflow.h>
+
+struct _p_FormPBPOLRAJAHIOPSPARSE{};
+typedef struct _p_FormPBPOLRAJAHIOPSPARSE *PBPOLRAJAHIOPSPARSE;
+
+extern PetscErrorCode OPFLOWSetVariableBoundsArray_PBPOLRAJAHIOPSPARSE(OPFLOW,double*,double*);
+extern PetscErrorCode OPFLOWSetConstraintBoundsArray_PBPOLRAJAHIOPSPARSE(OPFLOW,double*,double*);
+extern PetscErrorCode OPFLOWSetInitialGuessArray_PBPOLRAJAHIOPSPARSE(OPFLOW,double*);
+extern PetscErrorCode OPFLOWComputeEqualityConstraintsArray_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,double*);
+extern PetscErrorCode OPFLOWComputeInequalityConstraintsArray_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,double*);
+extern PetscErrorCode OPFLOWComputeObjectiveArray_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,double*);
+extern PetscErrorCode OPFLOWComputeGradientArray_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,double*);
+extern PetscErrorCode OPFLOWComputeSparseEqualityConstraintJacobian_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,int*,int*,double*);
+extern PetscErrorCode OPFLOWComputeSparseInequalityConstraintJacobian_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,int*,int*,double*);
+extern PetscErrorCode OPFLOWComputeSparseHessian_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,const double*,int*,int*,double*);
+extern PetscErrorCode OPFLOWComputeDenseEqualityConstraintJacobian_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,double*);
+extern PetscErrorCode OPFLOWComputeDenseInequalityConstraintJacobian_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,double*);
+extern PetscErrorCode OPFLOWComputeDenseHessian_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,const double*,double*);
+
+#endif
+#endif
+#endif
