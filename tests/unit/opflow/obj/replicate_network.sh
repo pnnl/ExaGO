79b04c9a7004d30443ad9b7c9f86b8b3858e05fd
rcrutherford <robert.rutherford@pnnl.gov>
2021-06-02 13:39:12 -0700

    Getting network size as input

--- tests/unit/opflow/obj/replicate_network.sh
+++ tests/unit/opflow/obj/replicate_network.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# Get the input for the size of the network:
-#   Make sure an input is given
-#       Return early with a message if not
+# Authors:
+#   - Cameron Rutherford <cameron.rutherford@pnnl.gov>
+
+# Returning early if no network size if given
+if [ $# -eq 0 ]; then
+  echo "No network size given. Call this script with a desired network size."
+  exit 1
+fi
+
+network_size=$1
+
 #   Replicate the network in this dir, and output file
 
