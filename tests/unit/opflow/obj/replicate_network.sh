d09031c611ffbe15fefd159b9c32eb9cfca1fe17
rcrutherford <robert.rutherford@pnnl.gov>
2021-06-02 13:28:01 -0700

    Add initial replication script.

---
+++ tests/unit/opflow/obj/replicate_network.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Get the input for the size of the network:
+#   Make sure an input is given
+#       Return early with a message if not
+#   Replicate the network in this dir, and output file
+
