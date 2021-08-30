333ad3bccf8f61cb375a938dc9bc03b1a17a3885
Asher Mancinelli <ashermancinelli@gmail.com>
2021-08-29 21:04:55 -0600

    Add spack env and support scripts

---
+++ buildsystem/container/install-deps-rhel-ubi8.sh
@@ -0,0 +1,3 @@
+yum install -y gcc gcc-c++ gcc-gfortran \
+  python3 git cmake make tar bzip2 xz patch \
+  automake libevent openssl openssl-devel openssl-libs
---
+++ buildsystem/container/install-spack-environment.sh
@@ -0,0 +1,15 @@
+git clone --branch ${SPACK_VERSION} ${SPACK_REPO} ${SPACK_INSTALL_LOCATION}
+. "${SPACK_INSTALL_LOCATION}/share/spack/setup-env.sh"
+spack env activate .
+
+# Find a few system packages to improve build times
+for pkg in cmake gmake python git; do
+  spack external find $pkg
+done
+spack compiler find
+
+# Show the full concretization
+spack concretize -f
+spack install --verbose --source --keep-stage -j `nproc`
+
+spack test exago
