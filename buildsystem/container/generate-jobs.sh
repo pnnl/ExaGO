932ca0895cbb106ee6be6e5a7a364086a5c2e451
Asher Mancinelli <ashermancinelli@gmail.com>
2021-08-31 16:49:44 -0600

    test for child pipelines

---
+++ buildsystem/container/generate-jobs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+set -x
+for filename in *.yaml
+do
+  export environment="${filename%.*}"
+  cp $filename spack.yaml
+  export dockerfile="Dockerfile.${environment}"
+  echo "spack containerize > '${dockerfile}'"
+  cat <<EOD
+job_${environment}:
+  stage: spack-container-build
+  tags: [k8s, ikp, exasgd, basic]
+  image: ubuntu:20.04
+  script:
+    |
+    echo "Building dockerfile for ${environment}"
+EOD
+  rm spack.yaml
+  echo "rm $dockerfile"
+done
