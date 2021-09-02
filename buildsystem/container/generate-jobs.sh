dfc1b949d100153bebb377dfd9aa65596479cda8
Asher Mancinelli <ashermancinelli@gmail.com>
2021-09-02 17:11:22 -0600

    Correctly generate jobs

--- buildsystem/container/generate-jobs.sh
+++ buildsystem/container/generate-jobs.sh
@@ -11,15 +11,16 @@ EOD
 
 find environments -type d -depth 1 | while read -r environment
 do
+  name=$(basename $environment)
   # Generate gitlab-ci job for container build
   cat <<EOD
-${environment}-build:
+${name}-build:
   stage: build
   tags: [k8s, ikp, exasgd, basic]
   extends:
     - .pnnllib-gitlab-build-container-image
   before_script:
-    - cp buildsystem/container/environments/$environment/Dockerfile Dockerfile
+    - cp buildsystem/container/${environment}/Dockerfile Dockerfile
   needs:
     - pipeline: \$PARENT_PIPELINE_ID
       job: spack-generate-job
