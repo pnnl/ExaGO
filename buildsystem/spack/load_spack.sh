25e44bd7b9eab7d1c5dbad40d7c910853b6ef0c6
Jaelyn Litzinger <jaelyn.litzinger@pnnl.gov>
2022-09-12 14:06:51 -0400

    add spack as a submodule

---
+++ buildsystem/spack/load_spack.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#assume running from exago root dir
+
+source ./tpl/spack/share/spack/setup-env.sh
+
+SPACKENV=$(pwd)/spack-env
+
+mkdir -p $SPACKENV
+cp ./buildsystem/clang-hip/crusher/spack.yaml $SPACKENV
+
+spack env create -d $SPACKENV
+spack env activate $SPACKENV
+
+spack concretize -f
