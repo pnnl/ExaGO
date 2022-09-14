e7168f83f0d94e6ecca6f87fe7edfea0b1311dab
rcrutherford <robert.rutherford@pnnl.gov>
2022-09-14 13:25:46 -0400

    Fix module path for exago only modules.

--- buildsystem/clang-hip/crusher/exagoOptimizedSpack.sh
+++ buildsystem/clang-hip/crusher/exagoOptimizedSpack.sh
@@ -3,7 +3,7 @@ export PROJ_DIR=/autofs/nccs-svm1_proj/csc359
 
 module purge
 
-module use -a /autofs/nccs-svm1_proj/csc359/cameron/spack/share/spack/test-modules/cray-sles15-zen3/
+module use -a /gpfs/alpine/proj-shared/csc359/cameron/spack-install/test-modules/cray-sles15-zen3/
 
 # Spack modules
 export SRCDIR=${SRCDIR:-$PWD}
