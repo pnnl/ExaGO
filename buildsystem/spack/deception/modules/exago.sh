950fcfc35e1e7b731ad83f30c2a5fade62704d4e
spack-auto-module <spack.bot@no-reply.com>
2023-01-06 22:38:08 +0000

    Update deception spack built modules - CI AUTOMATED MESSAGE.

--- buildsystem/spack/deception/modules/exago.sh
+++ buildsystem/spack/deception/modules/exago.sh
@@ -1,3 +1,3 @@
 module use -a /qfs/projects/exasgd/src/ci-deception/ci-modules/linux-centos7-zen2
-# exago@develop%gcc@10.2.0+cuda+hiop~ipo+ipopt+mpi+python+raja~rocm build_system=cmake build_type=RelWithDebInfo cuda_arch=60,70,75,80 dev_path=/people/svcexasgd/gitlab/18744/spack_deception arch=linux-centos7-zen2
-module load exago-develop-gcc-10.2.0-7p4lovy
+# exago@develop%gcc@10.2.0+cuda+hiop~ipo+ipopt+logging+mpi+python+raja~rocm build_system=cmake build_type=RelWithDebInfo cuda_arch=60,70,75,80 dev_path=/people/svcexasgd/gitlab/18977/spack_deception arch=linux-centos7-zen2
+module load exago-develop-gcc-10.2.0-rypc2n4
