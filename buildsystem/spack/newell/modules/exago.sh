c6b10b59ea2043250f203dab63ce9ee1a3667dfa
Rutherford, Cameron <robert.rutherford@pnnl.gov>
2023-02-08 21:05:31 +0000

    Update Ginkgo in CI pipelines

--- buildsystem/spack/newell/modules/exago.sh
+++ buildsystem/spack/newell/modules/exago.sh
@@ -1,3 +1,3 @@
 module use -a /qfs/projects/exasgd/src/ci-newll/ci-modules/linux-centos8-power9le
-# exago@develop%gcc@8.5.0+cuda~full_optimizations+hiop~ipo+ipopt+mpi+python+raja~rocm build_type=RelWithDebInfo cuda_arch=70 dev_path=/people/svcexasgd/gitlab/17410/spack_newell arch=linux-centos8-power9le
-module load exago-develop-gcc-8.5.0-knxr5qo
+# exago@develop%gcc@8.5.0+cuda+hiop~ipo+ipopt+mpi+python+raja~rocm build_system=cmake build_type=RelWithDebInfo cuda_arch=70 dev_path=/people/svcexasgd/gitlab/19492/spack_newell arch=linux-centos8-power9le
+module load exago-develop-gcc-8.5.0-hcx3idj
