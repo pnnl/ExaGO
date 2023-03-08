1c9495d56e1dc0cc2547cb238a2f80883c55dcbd
rcrutherford <robert.rutherford@pnnl.gov>
2023-03-07 20:52:42 -0500

    Update spack and add crusher environment.

--- buildsystem/spack/crusher/env.sh
+++ buildsystem/spack/crusher/env.sh
@@ -5,6 +5,15 @@ module reset
 # Configure python
 module load cray-python/3.9.12.1 
 
+# Configure base modules
+module load PrgEnv-cray
+module load craype-x86-trento
+module load craype-accel-amd-gfx90a
+module load cce/15.0.0
+module load rocm/5.4.0
+module load cray-mpich/8.1.23
+module load libfabric
+
 BASE=/gpfs/alpine/proj-shared/csc359/$(whoami)
 
 export SPACK_INSTALL=/gpfs/alpine/proj-shared/csc359/cameron/spack-install
