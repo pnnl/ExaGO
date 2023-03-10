5b7b2ebdfb8ad4e6d1230bd486beac33412673a3
rcrutherford <robert.rutherford@pnnl.gov>
2023-03-09 23:47:32 -0500

    Update crusher modules.

--- buildsystem/spack/crusher/modules/dependencies.sh
+++ buildsystem/spack/crusher/modules/dependencies.sh
@@ -1,4 +1,4 @@
-module use -a /gpfs/alpine/proj-shared/csc359/cameron/spack-install/test-modules/linux-sles15-zen3
+module use -a /ccs/proj/csc359/spack-install/modules/linux-sles15-zen3
 # pkgconf@1.8.0%cce@14.0.2-rocm5.2.0 build_system=autotools arch=linux-sles15-zen3
 module load pkgconf-1.8.0-cce-14.0.2-rocm5.2.0-qsabajr
 # ncurses@6.4%cce@14.0.2-rocm5.2.0~symlinks+termlib abi=none build_system=autotools arch=linux-sles15-zen3
@@ -27,8 +27,8 @@ module load camp-0.2.3-cce-14.0.2-rocm5.2.0-zm5udxk
 module load cray-mpich-8.1.23-cce-14.0.2-rocm5.2.0-hu32rmr
 # openblas@0.3.21%gcc@11.2.0~bignuma~consistent_fpcsr+fortran~ilp64+locking+pic+shared build_system=makefile patches=114f95f,d3d9b15 symbol_suffix=none threads=none arch=linux-sles15-zen3
 module load openblas-0.3.21-gcc-11.2.0-jinq2os
-# coinhsl@2019.05.21%gcc@11.2.0+blas build_system=autotools arch=linux-sles15-zen3
-module load coinhsl-2019.05.21-gcc-11.2.0-xzjojvz
+# coinhsl@2019.05.21%cce@14.0.2-rocm5.2.0+blas build_system=autotools arch=linux-sles15-zen3
+module load coinhsl-2019.05.21-cce-14.0.2-rocm5.2.0-yjak35z
 # hipblas@5.2.0%cce@14.0.2-rocm5.2.0~ipo build_system=cmake build_type=Release arch=linux-sles15-zen3
 module load hipblas-5.2.0-cce-14.0.2-rocm5.2.0-s5cqs75
 # hipsparse@5.2.0%cce@14.0.2-rocm5.2.0~ipo build_system=cmake build_type=Release arch=linux-sles15-zen3
@@ -47,37 +47,53 @@ module load magma-2.6.2-cce-14.0.2-rocm5.2.0-o2pvirv
 module load metis-5.1.0-cce-14.0.2-rocm5.2.0-7ssoebe
 # raja@0.14.0%cce@14.0.2-rocm5.2.0~cuda+examples+exercises~ipo~openmp+rocm+shared~tests amdgpu_target=gfx90a build_system=cmake build_type=RelWithDebInfo arch=linux-sles15-zen3
 module load raja-0.14.0-cce-14.0.2-rocm5.2.0-bysdt3z
-# libiconv@1.17%gcc@11.2.0 build_system=autotools libs=shared,static arch=linux-sles15-zen3
-module load libiconv-1.17-gcc-11.2.0-hr2sqld
+# libiconv@1.17%cce@14.0.2-rocm5.2.0 build_system=autotools libs=shared,static arch=linux-sles15-zen3
+module load libiconv-1.17-cce-14.0.2-rocm5.2.0-xcxmbl6
 # diffutils@3.8%cce@14.0.2-rocm5.2.0 build_system=autotools arch=linux-sles15-zen3
-module load diffutils-3.8-cce-14.0.2-rocm5.2.0-aboamqj
+module load diffutils-3.8-cce-14.0.2-rocm5.2.0-fbdahwf
 # libsigsegv@2.13%cce@14.0.2-rocm5.2.0 build_system=autotools arch=linux-sles15-zen3
 module load libsigsegv-2.13-cce-14.0.2-rocm5.2.0-3td32vd
 # m4@1.4.19%cce@14.0.2-rocm5.2.0+sigsegv build_system=autotools patches=9dc5fbd,bfdffa7 arch=linux-sles15-zen3
-module load m4-1.4.19-cce-14.0.2-rocm5.2.0-izkklm2
+module load m4-1.4.19-cce-14.0.2-rocm5.2.0-dlvx6nt
 # autoconf@2.69%cce@14.0.2-rocm5.2.0 build_system=autotools patches=35c4492,7793209,a49dd5b arch=linux-sles15-zen3
-module load autoconf-2.69-cce-14.0.2-rocm5.2.0-3puvqsu
+module load autoconf-2.69-cce-14.0.2-rocm5.2.0-cuo5cnh
 # automake@1.16.5%cce@14.0.2-rocm5.2.0 build_system=autotools arch=linux-sles15-zen3
-module load automake-1.16.5-cce-14.0.2-rocm5.2.0-nj5nhep
+module load automake-1.16.5-cce-14.0.2-rocm5.2.0-op7j6iy
 # libtool@2.4.7%cce@14.0.2-rocm5.2.0 build_system=autotools arch=linux-sles15-zen3
-module load libtool-2.4.7-cce-14.0.2-rocm5.2.0-anakvan
+module load libtool-2.4.7-cce-14.0.2-rocm5.2.0-q5k4f3s
 # gmp@6.2.1%cce@14.0.2-rocm5.2.0+cxx build_system=autotools libs=shared,static arch=linux-sles15-zen3
-module load gmp-6.2.1-cce-14.0.2-rocm5.2.0-cdvrmyk
+module load gmp-6.2.1-cce-14.0.2-rocm5.2.0-wmshlq6
 # autoconf-archive@2022.02.11%cce@14.0.2-rocm5.2.0 build_system=autotools patches=139214f arch=linux-sles15-zen3
 module load autoconf-archive-2022.02.11-cce-14.0.2-rocm5.2.0-ayr7xya
-# mpfr@4.0.2%cce@14.0.2-rocm5.2.0 build_system=autotools libs=shared,static patches=3f80b83 arch=linux-sles15-zen3
-module load mpfr-4.0.2-cce-14.0.2-rocm5.2.0-m2yj5wh
+# bzip2@1.0.8%cce@14.0.2-rocm5.2.0~debug~pic+shared build_system=generic arch=linux-sles15-zen3
+module load bzip2-1.0.8-cce-14.0.2-rocm5.2.0-ix24bpa
+# xz@5.4.1%cce@14.0.2-rocm5.2.0~pic build_system=autotools libs=shared,static arch=linux-sles15-zen3
+module load xz-5.4.1-cce-14.0.2-rocm5.2.0-hta7qck
+# libxml2@2.10.3%cce@14.0.2-rocm5.2.0~python build_system=autotools arch=linux-sles15-zen3
+module load libxml2-2.10.3-cce-14.0.2-rocm5.2.0-5mabs3o
+# pigz@2.7%cce@14.0.2-rocm5.2.0 build_system=makefile arch=linux-sles15-zen3
+module load pigz-2.7-cce-14.0.2-rocm5.2.0-gwoyvsn
+# zstd@1.5.2%cce@14.0.2-rocm5.2.0+programs build_system=makefile compression=none libs=shared,static arch=linux-sles15-zen3
+module load zstd-1.5.2-cce-14.0.2-rocm5.2.0-ziptnei
+# tar@1.34%cce@14.0.2-rocm5.2.0 build_system=autotools zip=pigz arch=linux-sles15-zen3
+module load tar-1.34-cce-14.0.2-rocm5.2.0-siw66qi
+# gettext@0.21.1%cce@14.0.2-rocm5.2.0+bzip2+curses+git~libunistring+libxml2+tar+xz build_system=autotools arch=linux-sles15-zen3
+module load gettext-0.21.1-cce-14.0.2-rocm5.2.0-seigj5f
+# texinfo@7.0%cce@14.0.2-rocm5.2.0 build_system=autotools arch=linux-sles15-zen3
+module load texinfo-7.0-cce-14.0.2-rocm5.2.0-azboikb
+# mpfr@4.2.0%cce@14.0.2-rocm5.2.0 build_system=autotools libs=shared,static arch=linux-sles15-zen3
+module load mpfr-4.2.0-cce-14.0.2-rocm5.2.0-tvir4fd
 # suite-sparse@5.13.0%cce@14.0.2-rocm5.2.0~cuda~graphblas~openmp+pic build_system=generic arch=linux-sles15-zen3
-module load suite-sparse-5.13.0-cce-14.0.2-rocm5.2.0-4pyyni7
+module load suite-sparse-5.13.0-cce-14.0.2-rocm5.2.0-sztzlr4
 # umpire@6.0.0%cce@14.0.2-rocm5.2.0+c~cuda~device_alloc~deviceconst+examples~fortran~ipo~numa~openmp+rocm+shared amdgpu_target=gfx90a build_system=cmake build_type=RelWithDebInfo tests=none arch=linux-sles15-zen3
 module load umpire-6.0.0-cce-14.0.2-rocm5.2.0-xx5s3oq
 # hiop@develop%cce@14.0.2-rocm5.2.0~cuda+deepchecking+ginkgo~ipo~jsrun+kron+mpi+raja+rocm~shared+sparse amdgpu_target=gfx90a build_system=cmake build_type=RelWithDebInfo arch=linux-sles15-zen3
-module load hiop-develop-cce-14.0.2-rocm5.2.0-frlsucb
+module load hiop-develop-cce-14.0.2-rocm5.2.0-yzb53tl
 # ipopt@3.12.10%cce@14.0.2-rocm5.2.0+coinhsl+debug~metis~mumps build_system=autotools arch=linux-sles15-zen3
-module load ipopt-3.12.10-cce-14.0.2-rocm5.2.0-ik6k2cl
+module load ipopt-3.12.10-cce-14.0.2-rocm5.2.0-2uz4kah
 # python@3.9.12%cce@14.0.2-rocm5.2.0+bz2+crypt+ctypes+dbm~debug+libxml2+lzma~nis~optimizations+pic+pyexpat+pythoncmd+readline+shared+sqlite3+ssl~tkinter+uuid+zlib build_system=generic patches=0d98e93,4c24573,f2fd060 arch=linux-sles15-zen3
 module load python-3.9.12-cce-14.0.2-rocm5.2.0-bhb3tpd
 # petsc@3.18.3%cce@14.0.2-rocm5.2.0~X+batch~cgns~complex~cuda~debug+double~exodusii~fftw+fortran~giflib~hdf5~hpddm~hwloc~hypre~int64~jpeg~knl~kokkos~libpng~libyaml~memkind~metis~mkl-pardiso~mmg~moab~mpfr+mpi~mumps~openmp~p4est~parmmg~ptscotch~random123~rocm~saws~scalapack+shared~strumpack~suite-sparse~superlu-dist~tetgen~trilinos~valgrind build_system=generic clanguage=C arch=linux-sles15-zen3
-module load petsc-3.18.3-cce-14.0.2-rocm5.2.0-pa6bpjx
+module load petsc-3.18.3-cce-14.0.2-rocm5.2.0-yi5wdkv
 # exago@develop%cce@14.0.2-rocm5.2.0~cuda+hiop~ipo+ipopt+logging+mpi~python+raja+rocm amdgpu_target=gfx90a build_system=cmake build_type=RelWithDebInfo dev_path=/ccs/home/rcruther/exago-git arch=linux-sles15-zen3
-## module load exago-develop-cce-14.0.2-rocm5.2.0-mnnl5zi
+## module load exago-develop-cce-14.0.2-rocm5.2.0-6jof7uv
