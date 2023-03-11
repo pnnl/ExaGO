aaa9fa9b86120d4a5a24054f6641e1db67a57e1f
rcrutherford <robert.rutherford@pnnl.gov>
2023-03-11 16:25:02 -0500

    Add failing fortran linked tests with cce compilers and Ipopt %gcc.

--- buildsystem/spack/crusher/modules/exago.sh
+++ buildsystem/spack/crusher/modules/exago.sh
@@ -1,3 +1,3 @@
 module use -a /ccs/proj/csc359/spack-install/modules/linux-sles15-zen3
-# exago@develop%gcc@11.2.0~cuda+hiop~ipo+ipopt+logging+mpi~python+raja+rocm amdgpu_target=gfx90a build_system=cmake build_type=RelWithDebInfo dev_path=/ccs/home/rcruther/gcc-exago-git arch=linux-sles15-zen3
-module load exago-develop-gcc-11.2.0-nuxnem6
+# exago@develop%cce@14.0.2-rocm5.2.0~cuda+hiop~ipo+ipopt+logging+mpi~python+raja+rocm amdgpu_target=gfx90a build_system=cmake build_type=RelWithDebInfo dev_path=/ccs/home/rcruther/exago-git arch=linux-sles15-zen3
+module load exago-develop-cce-14.0.2-rocm5.2.0-sy7mipq
