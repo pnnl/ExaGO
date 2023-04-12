8b77b37e71c6e550a326e4de0c86f4b6acf9b64a
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2023-04-12 12:49:52 -0400

    New dependencies after cray-mpich/8.1.25 upgrade

--- buildsystem/spack/crusher/modules/exago.sh
+++ buildsystem/spack/crusher/modules/exago.sh
@@ -1,3 +1,3 @@
 module use -a /ccs/proj/csc359/spack-install/modules/linux-sles15-zen3
-# exago@develop%clang@14.0.0-rocm5.2.0~cuda+hiop~ipo+ipopt+logging+mpi~python+raja+rocm amdgpu_target=gfx90a build_system=cmake build_type=RelWithDebInfo dev_path=/gpfs/alpine/scratch/nkouk/csc359/exago_crusher generator=make arch=linux-sles15-zen3
-module load exago-develop-clang-14.0.0-rocm5.2.0-wvg4yxx
+# exago@develop%clang@14.0.0-rocm5.2.0~cuda+hiop~ipo+ipopt+logging+mpi~python+raja+rocm amdgpu_target=gfx90a build_system=cmake build_type=RelWithDebInfo dev_path=/lustre/orion/scratch/nkouk/csc359/exago-frontier-amd generator=make arch=linux-sles15-zen3
+module load exago-develop-clang-14.0.0-rocm5.2.0-igdhdq4
