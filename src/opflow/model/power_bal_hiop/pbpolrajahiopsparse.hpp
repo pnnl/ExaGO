aa409e8d7adf68a066e98d6a4c6fd59136625a3c
William Perkins <william.perkins@pnnl.gov>
2023-05-23 08:28:26 -0700

    Code formatting

--- src/opflow/model/power_bal_hiop/pbpolrajahiopsparse.hpp
+++ src/opflow/model/power_bal_hiop/pbpolrajahiopsparse.hpp
@@ -8,22 +8,45 @@
 
 #include <opflow.h>
 
-struct _p_FormPBPOLRAJAHIOPSPARSE{};
+struct _p_FormPBPOLRAJAHIOPSPARSE {};
 typedef struct _p_FormPBPOLRAJAHIOPSPARSE *PBPOLRAJAHIOPSPARSE;
 
-extern PetscErrorCode OPFLOWSetVariableBoundsArray_PBPOLRAJAHIOPSPARSE(OPFLOW,double*,double*);
-extern PetscErrorCode OPFLOWSetConstraintBoundsArray_PBPOLRAJAHIOPSPARSE(OPFLOW,double*,double*);
-extern PetscErrorCode OPFLOWSetInitialGuessArray_PBPOLRAJAHIOPSPARSE(OPFLOW,double*);
-extern PetscErrorCode OPFLOWComputeEqualityConstraintsArray_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,double*);
-extern PetscErrorCode OPFLOWComputeInequalityConstraintsArray_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,double*);
-extern PetscErrorCode OPFLOWComputeObjectiveArray_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,double*);
-extern PetscErrorCode OPFLOWComputeGradientArray_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,double*);
-extern PetscErrorCode OPFLOWComputeSparseEqualityConstraintJacobian_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,int*,int*,double*);
-extern PetscErrorCode OPFLOWComputeSparseInequalityConstraintJacobian_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,int*,int*,double*);
-extern PetscErrorCode OPFLOWComputeSparseHessian_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,const double*,int*,int*,double*);
-extern PetscErrorCode OPFLOWComputeDenseEqualityConstraintJacobian_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,double*);
-extern PetscErrorCode OPFLOWComputeDenseInequalityConstraintJacobian_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,double*);
-extern PetscErrorCode OPFLOWComputeDenseHessian_PBPOLRAJAHIOPSPARSE(OPFLOW,const double*,const double*,double*);
+extern PetscErrorCode
+OPFLOWSetVariableBoundsArray_PBPOLRAJAHIOPSPARSE(OPFLOW, double *, double *);
+extern PetscErrorCode
+OPFLOWSetConstraintBoundsArray_PBPOLRAJAHIOPSPARSE(OPFLOW, double *, double *);
+extern PetscErrorCode OPFLOWSetInitialGuessArray_PBPOLRAJAHIOPSPARSE(OPFLOW,
+                                                                     double *);
+extern PetscErrorCode OPFLOWComputeEqualityConstraintsArray_PBPOLRAJAHIOPSPARSE(
+    OPFLOW, const double *, double *);
+extern PetscErrorCode
+OPFLOWComputeInequalityConstraintsArray_PBPOLRAJAHIOPSPARSE(OPFLOW,
+                                                            const double *,
+                                                            double *);
+extern PetscErrorCode
+OPFLOWComputeObjectiveArray_PBPOLRAJAHIOPSPARSE(OPFLOW, const double *,
+                                                double *);
+extern PetscErrorCode
+OPFLOWComputeGradientArray_PBPOLRAJAHIOPSPARSE(OPFLOW, const double *,
+                                               double *);
+extern PetscErrorCode
+OPFLOWComputeSparseEqualityConstraintJacobian_PBPOLRAJAHIOPSPARSE(
+    OPFLOW, const double *, int *, int *, double *);
+extern PetscErrorCode
+OPFLOWComputeSparseInequalityConstraintJacobian_PBPOLRAJAHIOPSPARSE(
+    OPFLOW, const double *, int *, int *, double *);
+extern PetscErrorCode OPFLOWComputeSparseHessian_PBPOLRAJAHIOPSPARSE(
+    OPFLOW, const double *, const double *, int *, int *, double *);
+extern PetscErrorCode
+OPFLOWComputeDenseEqualityConstraintJacobian_PBPOLRAJAHIOPSPARSE(OPFLOW,
+                                                                 const double *,
+                                                                 double *);
+extern PetscErrorCode
+OPFLOWComputeDenseInequalityConstraintJacobian_PBPOLRAJAHIOPSPARSE(
+    OPFLOW, const double *, double *);
+extern PetscErrorCode
+OPFLOWComputeDenseHessian_PBPOLRAJAHIOPSPARSE(OPFLOW, const double *,
+                                              const double *, double *);
 
 #endif
 #endif
