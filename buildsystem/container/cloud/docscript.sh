0c9ace61709700c2a80b66c3857aaa42447ea33d
sayefazad.sakin@pnnl.gov <sayefazad.sakin@pnnl.gov>
2023-07-31 18:04:11 -0700

    Add coinhsl into docscript [skip-ci]

--- buildsystem/container/cloud/docscript.sh
+++ buildsystem/container/cloud/docscript.sh
@@ -6,7 +6,7 @@ set -e
 source ../../../tpl/spack/share/spack/setup-env.sh
 echo "source to spack env is done"
 # Create new directory for spack environment and debugging
-SPACK_ENV=spack_env
+SPACK_ENV=spack_cloud_env
 mkdir -p $SPACK_ENV
 echo "created a dir named $SPACK_ENV"
 # Clean out the dir just in case
@@ -15,30 +15,27 @@ echo "Cleaned out the dir just in case"
 # Create spack directory based environment
 spack env create -d $SPACK_ENV
 echo "Created spack directory based environment"
-spack env activate -p spack_env
+spack env activate -p $SPACK_ENV
 echo "Activated spack directory based environment"
 
-# Content to be added to spack.yaml
-cat << EOF > ./spack_env/spack.yaml
-spack:
-  specs:
-    - cmake@3.26.3 arch=linux-amzn2-x86_64_v3
-    # - exago~mpi~hiop~ipopt+python
-  mirrors:
-    spack: https://binaries.spack.io/v0.20.1
-  container:
-    format: docker
-    images:
-      os: "amazonlinux:2"
-      spack: v0.20.1
-EOF
+cp spack.yaml $SPACK_ENV/
 
-cd spack_env
+cd $SPACK_ENV
 #create a docker file
 spack containerize > Dockerfile
 
+#copy the coinhsl, make sure to put the coinhsl  tar in the exago root directory
+cp ../../../../coinhsl-archive-2019.05.21.tar.gz $SPACK_ENV
+
+COIN_COPY="COPY coinhsl-archive-2019.05.21.tar.gz /opt/spack-environment/coinhsl-archive-2019.05.21.tar.gz"
+INSTALL_SOFTWARE_NOTE="# Install the software, remove unnecessary deps"
+sed -i "" "s|$INSTALL_SOFTWARE_NOTE|$COIN_COPY\n\n$INSTALL_SOFTWARE_NOTE|" Dockerfile
+
 # THIS ONLY WORKS ON MAC!!!
 sed -i "" "s/spack install/spack buildcache keys --install --trust \&\& spack install/g" Dockerfile
+
+# sed -i "" "s|CMD \[ \"/bin/bash\" \]|CMD \[ \"yum -y install libgomp\", \"yum -y install libgfortran\", \"/bin/bash\" \]|" Dockerfile
+# echo "CMD [ \"yum -y install libgomp\", \"yum -y install libgfortran\" ]" >> Dockerfile
 
 echo "Dockerfile created"
 # Build Docker image with Amazon Linux
@@ -47,7 +44,8 @@ docker build --progress plain -t $IMG_TAG .
 echo "Docker image '$IMG_TAG' built."
 # Run Docker container
 docker run --rm -i -t $IMG_TAG
-echo "Docker container 'exago_cont' ran successfully."
+echo "Docker container ran successfully."
+
 cd -
 
 set +e
