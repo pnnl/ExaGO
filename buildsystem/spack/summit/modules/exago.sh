ab6d6ae1299b04eedacf16cc744d04436d799363
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2023-09-27 11:50:49 -0400

    Relax constraints on exago dependencies on Summit

--- buildsystem/spack/summit/modules/exago.sh
+++ buildsystem/spack/summit/modules/exago.sh
@@ -1,3 +1,3 @@
 module use -a /gpfs/alpine/proj-shared/csc359/cameron/spack-install/summit-modules/linux-rhel8-power9le
-# exago@=develop%gcc@=10.2.0+cuda+hiop~ipo+ipopt+logging+mpi+python+raja~rocm build_system=cmake build_type=Debug cuda_arch=70 dev_path=/gpfs/alpine/scratch/nkouk/csc359/ExaGO-github generator=make arch=linux-rhel8-power9le
-module load exago/develop-gcc-10.2.0-k4i42sh
+# exago@=develop%gcc@=10.2.0+cuda+hiop~ipo+ipopt+logging+mpi+python+raja~rocm build_system=cmake build_type=Release cuda_arch=70 generator=make arch=linux-rhel8-power9le
+module load exago/develop-gcc-10.2.0-lpqzdxo
