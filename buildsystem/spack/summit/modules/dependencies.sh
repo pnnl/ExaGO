2d9e744edd6791423fe09b6c23b59b1ae3af85ef
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2023-09-28 11:57:21 -0400

    Add constraints on HiOp in the spack config. Part of the ExaGO package was conflicting with building HiOp in release mode.

--- buildsystem/spack/summit/modules/dependencies.sh
+++ buildsystem/spack/summit/modules/dependencies.sh
@@ -15,28 +15,52 @@ module load gnuconfig/2021-08-14-gcc-10.2.0-4gsxwpk
 module load gmake/4.4.1-gcc-10.2.0-m2nvote
 # openblas@=0.17.0%gcc@=10.2.0~bignuma~consistent_fpcsr+fortran~ilp64+locking+pic+shared symbol_suffix=none threads=none arch=linux-rhel8-power9le
 module load openblas/0.17.0-gcc-10.2.0-qcs6vhe
+# coinhsl@=2015.06.23%gcc@=10.2.0+blas arch=linux-rhel8-power9le
+module load coinhsl/2015.06.23-gcc-10.2.0-vctp77v
+# ginkgo@=1.5.0.glu_experimental%gcc@=10.2.0+cuda~develtools~full_optimizations~hwloc~ipo~mpi~oneapi+openmp~rocm+shared build_system=cmake build_type=Release cuda_arch=70 generator=make arch=linux-rhel8-power9le
+module load ginkgo/1.5.0.glu_experimental-gcc-10.2.0-4wicfxa
 # magma@=2.6.2%gcc@=10.2.0+cuda+fortran~ipo~rocm+shared build_type=Release cuda_arch=70 arch=linux-rhel8-power9le
 module load magma/2.6.2-gcc-10.2.0-wfpnmgj
+# metis@=5.1.0%gcc@=10.2.0~gdb~int64~real64+shared build_type=Release patches=4991da9,b1225da arch=linux-rhel8-power9le
+module load metis/5.1.0-gcc-10.2.0-lpkktw5
 # raja@=0.14.0%gcc@=10.2.0+cuda+examples+exercises~ipo+openmp~rocm+shared~tests build_type=Release cuda_arch=70 arch=linux-rhel8-power9le
 module load raja/0.14.0-gcc-10.2.0-r7kozj7
 # spectrum-mpi@=10.4.0.3-20210112%gcc@=10.2.0 arch=linux-rhel8-power9le
 module load spectrum-mpi/10.4.0.3-20210112-gcc-10.2.0-rhfl5sr
-# umpire@=6.0.0%gcc@=10.2.0~c+cuda~device_alloc~deviceconst+examples~fortran~ipo~numa~openmp~rocm~shared build_type=Release cuda_arch=70 tests=none arch=linux-rhel8-power9le
-module load umpire/6.0.0-gcc-10.2.0-eaxxvet
-# hiop@=1.0.0%gcc@=10.2.0+cuda~cusolver_lu~deepchecking~ginkgo~ipo~jsrun~kron+mpi+raja~rocm~shared~sparse build_system=cmake build_type=Release cuda_arch=70 generator=make arch=linux-rhel8-power9le
-module load hiop/1.0.0-gcc-10.2.0-pomojvf
-# coinhsl@=2015.06.23%gcc@=10.2.0+blas arch=linux-rhel8-power9le
-module load coinhsl/2015.06.23-gcc-10.2.0-vctp77v
-# metis@=5.1.0%gcc@=10.2.0~gdb~int64~real64+shared build_type=Release patches=4991da9,b1225da arch=linux-rhel8-power9le
-module load metis/5.1.0-gcc-10.2.0-lpkktw5
-# pkgconf@=1.8.0%gcc@=10.2.0 arch=linux-rhel8-power9le
-module load pkgconf/1.8.0-gcc-10.2.0-ygtljd4
-# ipopt@=3.12.10%gcc@=10.2.0+coinhsl~debug+metis~mumps arch=linux-rhel8-power9le
-module load ipopt/3.12.10-gcc-10.2.0-zl5xybc
 # libiconv@=1.16%gcc@=10.2.0 libs=shared,static arch=linux-rhel8-power9le
 module load libiconv/1.16-gcc-10.2.0-wyjuzaw
 # diffutils@=3.8%gcc@=10.2.0 arch=linux-rhel8-power9le
 module load diffutils/3.8-gcc-10.2.0-gkkihj7
+# libsigsegv@=2.13%gcc@=10.2.0 arch=linux-rhel8-power9le
+module load libsigsegv/2.13-gcc-10.2.0-7j4qaaw
+# m4@=1.4.19%gcc@=10.2.0+sigsegv patches=9dc5fbd,bfdffa7 arch=linux-rhel8-power9le
+module load m4/1.4.19-gcc-10.2.0-meue3ml
+# perl@=5.30.1%gcc@=10.2.0+cpanm+shared+threads arch=linux-rhel8-power9le
+module load perl/5.30.1-gcc-10.2.0-y7j4yqj
+# autoconf@=2.69%gcc@=10.2.0 patches=35c4492,7793209,a49dd5b arch=linux-rhel8-power9le
+module load autoconf/2.69-gcc-10.2.0-c2dqrc4
+# automake@=1.16.5%gcc@=10.2.0 arch=linux-rhel8-power9le
+module load automake/1.16.5-gcc-10.2.0-f5m2fqi
+# libtool@=2.4.7%gcc@=10.2.0 arch=linux-rhel8-power9le
+module load libtool/2.4.7-gcc-10.2.0-n2qpycy
+# gmp@=6.2.1%gcc@=10.2.0 libs=shared,static arch=linux-rhel8-power9le
+module load gmp/6.2.1-gcc-10.2.0-xexdp7f
+# autoconf-archive@=2022.02.11%gcc@=10.2.0 patches=139214f arch=linux-rhel8-power9le
+module load autoconf-archive/2022.02.11-gcc-10.2.0-6owwhwa
+# texinfo@=6.5%gcc@=10.2.0 patches=12f6edb,1732115 arch=linux-rhel8-power9le
+module load texinfo/6.5-gcc-10.2.0-5zrurw2
+# mpfr@=4.1.0%gcc@=10.2.0 libs=shared,static arch=linux-rhel8-power9le
+module load mpfr/4.1.0-gcc-10.2.0-4c6rm3n
+# suite-sparse@=5.10.1%gcc@=10.2.0~cuda~graphblas~openmp+pic~tbb arch=linux-rhel8-power9le
+module load suite-sparse/5.10.1-gcc-10.2.0-kmwoegj
+# umpire@=6.0.0%gcc@=10.2.0~c+cuda~device_alloc~deviceconst+examples~fortran~ipo~numa~openmp~rocm~shared build_type=Release cuda_arch=70 tests=none arch=linux-rhel8-power9le
+module load umpire/6.0.0-gcc-10.2.0-eaxxvet
+# hiop@=1.0.0%gcc@=10.2.0+cuda+cusolver_lu~deepchecking+ginkgo~ipo~jsrun+kron+mpi+raja~rocm~shared+sparse build_system=cmake build_type=Release cuda_arch=70 generator=make arch=linux-rhel8-power9le
+module load hiop/1.0.0-gcc-10.2.0-42wy3zk
+# pkgconf@=1.8.0%gcc@=10.2.0 arch=linux-rhel8-power9le
+module load pkgconf/1.8.0-gcc-10.2.0-ygtljd4
+# ipopt@=3.12.10%gcc@=10.2.0+coinhsl~debug+metis~mumps arch=linux-rhel8-power9le
+module load ipopt/3.12.10-gcc-10.2.0-zl5xybc
 # parmetis@=4.0.3%gcc@=10.2.0~gdb~int64~ipo+shared build_type=RelWithDebInfo patches=4f89253,50ed208,704b84f arch=linux-rhel8-power9le
 module load parmetis/4.0.3-gcc-10.2.0-by3w24c
 # python@=3.8.10%gcc@=10.2.0+bz2+ctypes+dbm~debug+libxml2+lzma~nis~optimizations+pic+pyexpat+pythoncmd+readline+shared+sqlite3+ssl~tix~tkinter~ucs4+uuid+zlib patches=0d98e93,4c24573,f2fd060 arch=linux-rhel8-power9le
@@ -57,10 +81,6 @@ module load py-cython/0.29.36-gcc-10.2.0-futh2n7
 module load py-mpi4py/3.1.4-gcc-10.2.0-giulifn
 # py-flit-core@=3.9.0%gcc@=10.2.0 build_system=python_pip arch=linux-rhel8-power9le
 module load py-flit-core/3.9.0-gcc-10.2.0-rkvgviw
-# libsigsegv@=2.13%gcc@=10.2.0 arch=linux-rhel8-power9le
-module load libsigsegv/2.13-gcc-10.2.0-7j4qaaw
-# m4@=1.4.19%gcc@=10.2.0+sigsegv patches=9dc5fbd,bfdffa7 arch=linux-rhel8-power9le
-module load m4/1.4.19-gcc-10.2.0-meue3ml
 # perl@=5.30.1%gcc@=10.2.0+cpanm+opcode+open+shared+threads build_system=generic arch=linux-rhel8-power9le
 module load perl/5.30.1-gcc-10.2.0-jh4r2fu
 # autoconf@=2.69%gcc@=10.2.0 build_system=autotools patches=35c4492,7793209,a49dd5b arch=linux-rhel8-power9le
@@ -103,8 +123,6 @@ module load gettext/0.21.1-gcc-10.2.0-t5i6mgx
 module load libunistring/1.1-gcc-10.2.0-dbahaeb
 # libidn2@=2.3.4%gcc@=10.2.0 build_system=autotools arch=linux-rhel8-power9le
 module load libidn2/2.3.4-gcc-10.2.0-vydwqsc
-# libtool@=2.4.7%gcc@=10.2.0 arch=linux-rhel8-power9le
-module load libtool/2.4.7-gcc-10.2.0-n2qpycy
 # bison@=3.8.2%gcc@=10.2.0 build_system=autotools arch=linux-rhel8-power9le
 module load bison/3.8.2-gcc-10.2.0-ijrzj6d
 # findutils@=4.9.0%gcc@=10.2.0 build_system=autotools patches=440b954 arch=linux-rhel8-power9le
@@ -152,4 +170,4 @@ module load py-iniconfig/2.0.0-gcc-10.2.0-n5eln73
 # py-pytest@=7.3.2%gcc@=10.2.0 build_system=python_pip arch=linux-rhel8-power9le
 module load py-pytest/7.3.2-gcc-10.2.0-l2ofgsi
 # exago@=develop%gcc@=10.2.0+cuda+hiop~ipo+ipopt+logging+mpi+python+raja~rocm build_system=cmake build_type=Release cuda_arch=70 generator=make arch=linux-rhel8-power9le
-## module load exago/develop-gcc-10.2.0-lpqzdxo
+## module load exago/develop-gcc-10.2.0-2xhvhux
--- buildsystem/spack/summit/modules/exago.sh
+++ buildsystem/spack/summit/modules/exago.sh
@@ -1,3 +1,3 @@
 module use -a /gpfs/alpine/proj-shared/csc359/cameron/spack-install/summit-modules/linux-rhel8-power9le
 # exago@=develop%gcc@=10.2.0+cuda+hiop~ipo+ipopt+logging+mpi+python+raja~rocm build_system=cmake build_type=Release cuda_arch=70 generator=make arch=linux-rhel8-power9le
-module load exago/develop-gcc-10.2.0-lpqzdxo
+module load exago/develop-gcc-10.2.0-2xhvhux
