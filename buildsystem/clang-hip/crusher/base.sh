fbb7768196d47908d85ae303432db5c0ff0b268f
Nicholson Koukpaizan <72402802+nkoukpaizan@users.noreply.github.com>
2023-10-04 17:53:11 -0400

    Update Frontier modules (#22)
    * Use gfortran to compile Fortran code on Crusher/Frontier
    
    * Remove 'module reset' from crusher/env.sh for Spack
    
    * Just use hiop@0.7.2
    
    * Add Spack external spec for hiprand on Crusher/Frontier
    
    * Update Crusher/Frontier modules
    
    * Revert deleted AMDGPU_TARGETS extra arg on Crusher/Frontier
    
    * Apply pre-commmit fixes
    
    ---------
    
    Co-authored-by: nkoukpaizan <nkoukpaizan@users.noreply.github.com>

--- buildsystem/clang-hip/crusher/base.sh
+++ buildsystem/clang-hip/crusher/base.sh
@@ -3,16 +3,21 @@
 export MY_CLUSTER=crusher
 export PROJ_DIR=/autofs/nccs-svm1_proj/csc359
 
-module purge
+module reset
 
 # System modules
-module load rocm/5.2.0
-module load libfabric/1.15.0.0
+module load PrgEnv-gnu-amd
+module load craype-x86-trento
+module load craype-accel-amd-gfx90a
+module load amd-mixed/5.2.0
+module load gcc/12.2.0
+module load cray-mpich/8.1.25
+module load libfabric
 
 # Consider changing to $(which clang) as for deception
-export CC=/opt/rocm-5.2.0/llvm/bin/clang
-export CXX=/opt/rocm-5.2.0/llvm/bin/clang++
-export FC=/opt/rocm-5.2.0/llvm/bin/flang
+export CC=/opt/rocm-5.2.0/llvm/bin/amdclang
+export CXX=/opt/rocm-5.2.0/llvm/bin/amdclang++
+export FC=/opt/rocm-5.2.0/llvm/bin/amdflang
 
 export EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DEXAGO_CTEST_LAUNCH_COMMAND='srun'"
 export EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DAMDGPU_TARGETS='gfx90a'"
