6cd37e5abc30601c88ac413da6400273fb142958
Bruce J Palmer <d3g293@deception04.pnl.gov>
2023-10-06 08:37:20 -0700

    Applied additional changes to selfcheck.cpp file for PFLOW, SOPFLOW and SCOPFLOW to adapt tests for running on multiple MPI ranks.

--- tests/functionality/scopflow/selfcheck.cpp
+++ tests/functionality/scopflow/selfcheck.cpp
@@ -135,6 +135,39 @@ struct ScopflowFunctionalityTests
     if (err)
       throw ExaGOError("Error getting MPI rank number");
 
+#if 0
+    int n_preset_procs;
+    set_if_found(n_preset_procs, presets, "n_procs");
+    int n_testcase_procs = -1;
+    set_if_found(n_testcase_procs, testcase, "n_procs");
+
+    if (-1 != n_testcase_procs) {
+      if (my_rank == logging_rank) {
+        std::stringstream errs;
+        errs << "Number of processes should be declared globally in the preset "
+                "area of the test suite TOML file, not inside each testcase.\n"
+             << "Testcase: " << testcase << "\nWith presets:\n"
+             << presets;
+        throw ExaGOError(errs.str().c_str());
+      }
+      exit(0);
+    } else if (nprocs != n_preset_procs) {
+      if (my_rank == logging_rank) {
+        std::stringstream errs;
+        errs << "SCOPFLOW Functionality test suite found " << n_preset_procs
+             << " processes specified in the presets of the test suite TOML "
+                "file, but this test is being run with "
+             << nprocs << " processes.\nTestcase: " << testcase
+             << "\nWith presets:\n"
+             << presets;
+        throw ExaGOError(errs.str().c_str());
+      }
+      exit(0);
+      return;
+    }
+#endif
+
+
     auto ensure_option_available = [&](const std::string &opt) {
       bool is_available = testcase.contains(opt) || presets.contains(opt);
       if (!is_available) {
