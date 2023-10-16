6dcc18073aac2a5182e1f31314a370f7c42006c3
Cameron Rutherford <robert.rutherford@pnnl.gov>
2023-10-16 17:12:10 -0400

    Update exago_python.hpp to conditionally include mpi4py

--- interfaces/python/exago_python.hpp
+++ interfaces/python/exago_python.hpp
@@ -7,8 +7,11 @@
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
 
+#ifdef EXAGO_ENABLE_MPI
 #include <mpi.h>
 #include <mpi4py/mpi4py.h>
+#endif
+
 #include <utils.h>
 #include <string.h>
 
