95f0b866b0abf37bc43d3ef1dd2a40086010d63c
Jaelyn Litzinger <litzingj@login1.ascent.olcf.ornl.gov>
2023-10-17 18:37:38 -0400

    force external mpi

--- buildsystem/gcc-cuda/ascentVariables.sh
+++ buildsystem/gcc-cuda/ascentVariables.sh
@@ -7,3 +7,5 @@ source $SRCDIR/buildsystem/gcc-cuda/ascent/base.sh
 
 # Spack modules
 source $SRCDIR/buildsystem/spack/$MY_CLUSTER/modules/dependencies.sh
+
+module load spectrum-mpi/10.4.0.3-20210112
