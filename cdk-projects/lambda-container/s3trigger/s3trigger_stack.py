5ff136125864a207096a8b71006b4a4e60b710ab
jaya.mohan@pnnl.gov <moha907@we42155.pnl.gov>
2023-10-26 01:38:22 -0700

    software stack modified to pull image f rom ecr for lambda function

--- cdk-projects/lambda-container/s3trigger/s3trigger_stack.py
+++ cdk-projects/lambda-container/s3trigger/s3trigger_stack.py
@@ -13,24 +13,17 @@ class S3TriggerStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # create lambda function
-        # function = _lambda.Function(self, "lambda_function",
-        # runtime=_lambda.Runtime.PYTHON_3_7,
-        # handler="lambda-handler.main",
-        # code=_lambda.Code.from_asset("./lambda"))
-        # function=_lambda.DockerImageFunction(self, "ExagoLambdaFunction",
-        # code=_lambda.DockerImageCode.from_image_asset("lambda"))
-    ecr_repository = ecr.Repository(self, "ExagoRepo",
-                                    repository_name="exago_repo")  # Specify your repository name
+        ecr_repository = ecr.Repository.from_repository_name(self, "ExagoRepo",
+                                    repository_name="test_repo")  # Specify your repository name
 
     # Define the Lambda function using a Docker container from ECR
-    function = _lambda.DockerImageFunction(self, "ExagoLambdaFunction",
+        function = _lambda.DockerImageFunction(self, "ExagoLambdaFunction",
                                            code=_lambda.DockerImageCode.from_ecr(repository=ecr_repository))
     # create s3 bucket
-    s3 = _s3.Bucket(self, "s3bucket")
+        s3 = _s3.Bucket(self, "s3bucket")
 
     # create s3 notification for lambda function
-    notification = aws_s3_notifications.LambdaDestination(function)
+        notification = aws_s3_notifications.LambdaDestination(function)
 
     # assign notification for the s3 event type (ex: OBJECT_CREATED)
-    s3.add_event_notification(_s3.EventType.OBJECT_CREATED, notification)
+        s3.add_event_notification(_s3.EventType.OBJECT_CREATED, notification)
