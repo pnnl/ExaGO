008164e51543d06e15b15bd25774b8c16333b10b
Jaelyn Litzinger <93339979+jaelynlitz@users.noreply.github.com>
2024-02-23 15:22:03 -0800

    exclude fetching exago for spack mirror [deception-rebuild]
    Co-authored-by: Cameron Rutherford <robert.rutherford@pnnl.gov>

--- buildsystem/spack/binary_mirror.sh
+++ buildsystem/spack/binary_mirror.sh
@@ -5,7 +5,7 @@ SPACK_MIRROR="${SPACK_MIRROR:?SPACK_MIRROR is unset. Please use the load_spack s
 spack -e $SPACKENV develop --path=$(pwd) exago@develop &&
 spack -e $SPACKENV bootstrap now &&
 spack -e $SPACKENV concretize -f &&
-(spack -e $SPACKENV mirror create -a --directory $SPACK_MIRROR || true) &&
+spack -e $SPACKENV mirror create -a --exclude-specs exago@develop --directory $SPACK_MIRROR &&
 spack -e $SPACKENV mirror add local file://$SPACK_MIRROR &&
 spack -e $SPACKENV mirror list
 
