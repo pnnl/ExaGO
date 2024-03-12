e17ef0d6dfea740f78998929f529ce5a23f181d8
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2024-03-12 14:59:25 -0400

    Build on Frontier with fewer hacks and more recent Spack.

--- buildsystem/spack/crusher/modules/exago.sh
+++ buildsystem/spack/crusher/modules/exago.sh
@@ -1,3 +1,3 @@
 module use -a /lustre/orion/eng145/world-shared/spack-install/modules/linux-sles15-x86_64
 # exago@=develop%clang@=16.0.0-rocm5.6.0-mixed~cuda+hiop~ipo+ipopt+logging+mpi~python+raja+rocm amdgpu_target=gfx90a build_system=cmake build_type=Release generator=make arch=linux-sles15-x86_64
-module load exago/develop-clang-16.0.0-rocm5.6.0-mixed-6xcrrvo
+module load exago/develop-clang-16.0.0-rocm5.6.0-mixed-kqa5be2
