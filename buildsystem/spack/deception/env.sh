2f4e9aca8442a6a699cec9130af610163d13c0d4
rcrutherford <robert.rutherford@pnnl.gov>
2024-03-29 08:09:13 -0700

    Add small spack changes to cleanup spack process on Deception.

--- buildsystem/spack/deception/env.sh
+++ buildsystem/spack/deception/env.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# Just for CI
 source /etc/profile.d/modules.sh
-module purge
 
 # Load system python
-module load python/miniconda3.9
-source /share/apps/python/miniconda3.9/etc/profile.d/conda.sh
+module rm python
+module load python/miniconda3.9 >/dev/null 2>&1
+source /share/apps/python/miniconda3.9/etc/profile.d/conda.sh >/dev/null 2>&1
 
 # Load system modules
+module rm gcc
 module load gcc/9.1.0
+module rm cuda
 module load cuda/11.4
+module rm openmpi
 module load openmpi/4.1.0mlx5.0
 
 # Define environment variables for where spack stores key files
