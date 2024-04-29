c790be8256d141b12e2d281cbeeec2959842a161
Jaya Mohan <moha907@deception02.pnl.gov>
2024-04-29 16:22:48 -0700

    test tcopflow functions

--- tests/interfaces/python/test_5_tcopflow.py
+++ tests/interfaces/python/test_5_tcopflow.py
@@ -12,20 +12,62 @@ check_preconditions()
 
 exago_ignore = -1000000
 
-
-@pytest.mark.nocomm
-@pytest.mark.MPI
+''' cannot run just creating tcopflow object bc opflow sub objects
+   won't be allocated (done in TCOPFLOWSetUp) so destructor seg faults
 def test_creating_tcopflow():
-    '''Testing creation of tcopflow object'''
     tcopf = exago.TCOPFLOW()
+'''
 
 
+def run_tcopflow(solver):
+    tcopf = exago.TCOPFLOW()
+    path = exago.prefix()
+
+    tcopf.set_tolerance(1.0E-03)
+    tcopf.set_network_data(os.path.join(
+        path, 'share', 'exago', 'datafiles', 'case9', 'case9mod_gen3_wind.m'))
+    tcopf.set_solver(solver)
+    # set_time_step_and_duration
+    # set_load_profiles
+    # set_wind_gen_profiles
+
+    tcopf.setup()
+    tcopf.solve()
+
+    assert tcopf.get_convergence_status()
+
+    obj = tcopf.get_objective()
+    assert isinstance(obj, float)
+
+    n = tcopf.get_num_iterations()
+    assert isinstance(n, int)
+
+    # get_solution
+    # print_solution
+
+    oname = os.path.join(tempfile.gettempdir(), "tcopflow_test_solution.csv")
+    tcopf.save_solution(0, exago.OutputFormat.CSV, oname)
+    assert os.path.exists(oname)
+    os.unlink(oname)
+
+    oname = os.path.join(tempfile.gettempdir(), "tcopflow_test_solution.m")
+    tcopf.save_solution(0, exago.OutputFormat.MATPOWER, oname)
+    assert os.path.exists(oname)
+    os.unlink(oname)
+
+    oname = os.path.join(tempfile.gettempdir(), "tcopflow_test_solution_dir")
+    tcopf.save_solution_all(exago.OutputFormat.MATPOWER, oname)
+    assert os.path.exists(oname)
+    shutil.rmtree(oname)
+
+
+# From sopflow_multicontingency.toml:
+# description =
+# 'datafiles/case9/case9mod_gen3_wind.m IPOPT 3scen contingencies SOPFLOW'
 @pytest.mark.nocomm
 @pytest.mark.MPI
-def test_get_prefix():
-    '''Test retrieving datafile path'''
-    path = exago.prefix()
-    assert isinstance(path, str)
+def test_tcopflow_1():
+    run_tcopflow('IPOPT')
 
 
 ''' example
@@ -44,14 +86,3 @@ def test_set_model_and_solver():
     opf.read_mat_power_data(os.path.join(
         path, 'share', 'exago', 'datafiles', 'case9', 'case9mod.m'))
 '''
-
-
-@pytest.mark.nocomm
-@pytest.mark.MPI
-def test_set_tolerance():
-    '''test get/set tolerance for tcopflow'''
-    tcopf = exago.TCOPFLOW()
-    tcopf.set_tolerance(-6)
-    tolerance = tcopf.get_tolerance()
-    assert isinstance(tolerance, float)
-    assert tolerance == -6
