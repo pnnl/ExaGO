3b8b92179eefdd4bb5ed863673ed67539565338f
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2024-05-13 18:02:37 -0400

    Let Spack install cuda/11.4.2 on Summit. This seems to work, while the system's DefApps-2023 cuda/11.4.2 does not.

--- buildsystem/spack/summit/env.sh
+++ buildsystem/spack/summit/env.sh
@@ -3,9 +3,9 @@
 # Configure python
 module load python/3.8-anaconda3
 
-export SPACK_INSTALL=/gpfs/alpine2/stf006/world-shared/nkouk/parco-spack-install
+export SPACK_INSTALL=/gpfs/alpine2/stf006/world-shared/nkouk/parco-cuda11.4.2-spack-install
 export SPACK_MODULES=summit-modules
-export SPACK_CACHE=/gpfs/alpine2/stf006/world-shared/nkouk/parco-spack-cache
+export SPACK_CACHE=/gpfs/alpine2/stf006/world-shared/nkouk/parco-cuda11.4.2-spack-cache
 export SPACK_PYTHON=$OLCF_PYTHON_ANACONDA3_ROOT
 export SPACK_DISABLE_LOCAL_CONFIG=1
 
--- buildsystem/spack/summit/modules/cuda11.4.2.env.sh
+++ buildsystem/spack/summit/modules/cuda11.4.2.env.sh
@@ -1,32 +1,40 @@
-module use -a /gpfs/alpine2/stf006/world-shared/nkouk/parco-spack-install/summit-modules/linux-rhel8-power9le
+module use -a /gpfs/alpine2/stf006/world-shared/nkouk/parco-cuda11.4.2-spack-install/summit-modules/linux-rhel8-power9le
 # cmake@3.21.3%gcc@10.2.0~doc+ncurses+ownlibs~qt build_type=Release arch=linux-rhel8-power9le
 module load cmake-3.21.3-gcc-10.2.0-ijmjum5
 # blt@0.4.1%gcc@10.2.0 arch=linux-rhel8-power9le
 module load blt-0.4.1-gcc-10.2.0-ymjl6ab
 # cub@1.16.0%gcc@10.2.0 arch=linux-rhel8-power9le
 module load cub-1.16.0-gcc-10.2.0-a5uzxeq
-# cuda@11.4.2%gcc@10.2.0~allow-unsupported-compilers~dev arch=linux-rhel8-power9le
-module load cuda-11.4.2-gcc-10.2.0-4bvhk3u
-# camp@0.2.3%gcc@10.2.0+cuda~ipo+openmp~rocm~tests build_type=RelWithDebInfo cuda_arch=70 arch=linux-rhel8-power9le
-module load camp-0.2.3-gcc-10.2.0-fnpymoo
 # gnuconfig@2021-08-14%gcc@10.2.0 arch=linux-rhel8-power9le
 module load gnuconfig-2021-08-14-gcc-10.2.0-4gsxwpk
+# libiconv@1.16%gcc@10.2.0 libs=shared,static arch=linux-rhel8-power9le
+module load libiconv-1.16-gcc-10.2.0-wyjuzaw
+# pkgconf@1.8.0%gcc@10.2.0 arch=linux-rhel8-power9le
+module load pkgconf-1.8.0-gcc-10.2.0-ygtljd4
+# xz@5.2.5%gcc@10.2.0~pic libs=shared,static arch=linux-rhel8-power9le
+module load xz-5.2.5-gcc-10.2.0-x3b5hud
+# zlib@1.2.12%gcc@10.2.0+optimize+pic+shared patches=0d38234 arch=linux-rhel8-power9le
+module load zlib-1.2.12-gcc-10.2.0-j3g46dc
+# libxml2@2.10.1%gcc@10.2.0~python arch=linux-rhel8-power9le
+module load libxml2-2.10.1-gcc-10.2.0-wkkknmv
+# cuda@11.4.2%gcc@10.2.0~allow-unsupported-compilers~dev arch=linux-rhel8-power9le
+module load cuda-11.4.2-gcc-10.2.0-rxp5tdj
+# camp@0.2.3%gcc@10.2.0+cuda~ipo+openmp~rocm~tests build_type=RelWithDebInfo cuda_arch=70 arch=linux-rhel8-power9le
+module load camp-0.2.3-gcc-10.2.0-ufkgnzv
 # openblas@0.17.0%gcc@10.2.0~bignuma~consistent_fpcsr+fortran~ilp64+locking+pic+shared symbol_suffix=none threads=none arch=linux-rhel8-power9le
 module load openblas-0.17.0-gcc-10.2.0-qcs6vhe
 # coinhsl@2015.06.23%gcc@10.2.0+blas arch=linux-rhel8-power9le
 module load coinhsl-2015.06.23-gcc-10.2.0-vctp77v
 ## ginkgo@1.5.0.glu_experimental%gcc@10.2.0+cuda~develtools~full_optimizations~hwloc~ipo~oneapi+openmp~rocm+shared build_type=Release cuda_arch=70 arch=linux-rhel8-power9le
-#module load ginkgo-1.5.0.glu_experimental-gcc-10.2.0-mzvjjxc
+#module load ginkgo-1.5.0.glu_experimental-gcc-10.2.0-b3dpsqw
 # magma@2.6.2%gcc@10.2.0+cuda+fortran~ipo~rocm+shared build_type=Release cuda_arch=70 arch=linux-rhel8-power9le
-module load magma-2.6.2-gcc-10.2.0-wfpnmgj
+module load magma-2.6.2-gcc-10.2.0-lyw6xf2
 # metis@5.1.0%gcc@10.2.0~gdb~int64~real64+shared build_type=Release patches=4991da9,b1225da arch=linux-rhel8-power9le
 module load metis-5.1.0-gcc-10.2.0-lpkktw5
 # raja@0.14.0%gcc@10.2.0+cuda+examples+exercises~ipo+openmp~rocm+shared~tests build_type=Release cuda_arch=70 arch=linux-rhel8-power9le
-module load raja-0.14.0-gcc-10.2.0-r7kozj7
+module load raja-0.14.0-gcc-10.2.0-74waavg
 # spectrum-mpi@10.4.0.3-20210112%gcc@10.2.0 arch=linux-rhel8-power9le
 module load spectrum-mpi-10.4.0.3-20210112-gcc-10.2.0-rhfl5sr
-# libiconv@1.16%gcc@10.2.0 libs=shared,static arch=linux-rhel8-power9le
-module load libiconv-1.16-gcc-10.2.0-wyjuzaw
 # diffutils@3.8%gcc@10.2.0 arch=linux-rhel8-power9le
 module load diffutils-3.8-gcc-10.2.0-gkkihj7
 # libsigsegv@2.13%gcc@10.2.0 arch=linux-rhel8-power9le
@@ -52,17 +60,11 @@ module load mpfr-4.1.0-gcc-10.2.0-4c6rm3n
 # suite-sparse@5.10.1%gcc@10.2.0~cuda~graphblas~openmp+pic~tbb arch=linux-rhel8-power9le
 module load suite-sparse-5.10.1-gcc-10.2.0-kmwoegj
 # umpire@6.0.0%gcc@10.2.0~c+cuda~device_alloc~deviceconst+examples~fortran~ipo~numa~openmp~rocm~shared build_type=Release cuda_arch=70 tests=none arch=linux-rhel8-power9le
-module load umpire-6.0.0-gcc-10.2.0-eaxxvet
+module load umpire-6.0.0-gcc-10.2.0-k7f35xy
 ## hiop@0.7.2%gcc@10.2.0+cuda+cusolver+deepchecking+ginkgo~ipo~jsrun~kron+mpi+raja~rocm~shared+sparse build_type=RelWithDebInfo cuda_arch=70 arch=linux-rhel8-power9le
-#module load hiop-0.7.2-gcc-10.2.0-p35vph5
-# pkgconf@1.8.0%gcc@10.2.0 arch=linux-rhel8-power9le
-module load pkgconf-1.8.0-gcc-10.2.0-ygtljd4
+#module load hiop-0.7.2-gcc-10.2.0-wyjzs7e
 # ipopt@3.12.10%gcc@10.2.0+coinhsl~debug+metis~mumps arch=linux-rhel8-power9le
 module load ipopt-3.12.10-gcc-10.2.0-zl5xybc
-# libiconv@1.16%gcc@10.2.0 libs=shared,static arch=linux-rhel8-power9le
-module load libiconv-1.16-gcc-10.2.0-wyjuzaw
-# diffutils@3.8%gcc@10.2.0 arch=linux-rhel8-power9le
-module load diffutils-3.8-gcc-10.2.0-gkkihj7
 # parmetis@4.0.3%gcc@10.2.0~gdb~int64~ipo+shared build_type=RelWithDebInfo patches=4f89253,50ed208,704b84f arch=linux-rhel8-power9le
 module load parmetis-4.0.3-gcc-10.2.0-by3w24c
 # python@3.8.10%gcc@10.2.0+bz2+ctypes+dbm~debug+libxml2+lzma~nis~optimizations+pic+pyexpat+pythoncmd+readline+shared+sqlite3+ssl~tix~tkinter~ucs4+uuid+zlib patches=0d98e93,4c24573,f2fd060 arch=linux-rhel8-power9le
@@ -70,4 +72,4 @@ module load python-3.8.10-gcc-10.2.0-yeap5du
 # petsc@3.18.0%gcc@10.2.0~X~batch~cgns~complex~cuda~debug+double~exodusii~fftw+fortran~giflib~hdf5~hpddm~hwloc~hypre~int64~jpeg~knl~kokkos~libpng~libyaml~memkind+metis~mkl-pardiso~mmg~moab~mpfr+mpi~mumps~openmp~p4est~parmmg~ptscotch~random123~rocm~saws~scalapack+shared~strumpack~suite-sparse~superlu-dist~tetgen~trilinos~valgrind clanguage=C patches=2daeca7 arch=linux-rhel8-power9le
 module load petsc-3.18.0-gcc-10.2.0-6zx23r5
 # exago@parco%gcc@10.2.0+cuda+hiop~ipo+ipopt+mpi~python+raja~rocm build_type=RelWithDebInfo cuda_arch=70 arch=linux-rhel8-power9le
-## module load exago-parco-gcc-10.2.0-novruu5
+## module load exago-parco-gcc-10.2.0-fxukj2j
