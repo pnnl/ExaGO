f69d149afe49d0d012d0ac9dc10d0661ae0b79b0
Shri <shri@pnnl.gov>
2025-01-04 16:04:26 -0600

    Visualization of zones and areas

--- viz/geninputfile.py
+++ viz/geninputfile.py
@@ -12,6 +12,10 @@ with open('module_casedata.js', 'w') as f:
     f.write('\n')
     f.write('\t\t\t\tvar casedata0 = {};\n')
     f.write('\t\t\t\tcasedata0.geojsondata = {};\n')
+    f.write('\t\t\t\tcasedata0.nareas = inputcasedata.nareas;\n')
+    f.write('\t\t\t\tcasedata0.nzones = inputcasedata.nzones;\n')
+    f.write('\t\t\t\tcasedata0.areas = inputcasedata.areas;\n')
+    f.write('\t\t\t\tcasedata0.zones = inputcasedata.zones;\n')
     f.write('\t\t\t\tcasedata0.geojsondata.type = "FeatureCollection";\n')
     f.write(
         '\t\t\t\tcasedata0.geojsondata.features = [...inputcasedata.geojsondata.features];\n')
